02425a8bd93cf6e0decbb299e15f15a16ced5e47
Habfast <paul@ahead-solutions.ch>
2016-03-10 13:51:03 +0100

    WiP on estimation integration

--- app/scripts/app/models/review/estimation.controller.js
+++ app/scripts/app/models/review/estimation.controller.js
@@ -24,43 +24,32 @@ angular.module('chuvApp.models').controller('EstimationController',['$scope', '$
   $scope.estimate_model = function () {
     $scope.loading_estimation = true;
 
-    var child_scope = $scope.$new();
-    child_scope.parametrization = {};
-    child_scope.help_is_open = true;
-    child_scope.parametrization_is_correct = function () {
-      return true;
-      //return _.all(
-      //  child_scope.estimation.parameters,
-      //  function (parameter) { return !!child_scope.parametrization[parameter.id] }
-      //)
-    };
+    function _map (variable) {
+      return { "code": variable.code };
+    }
 
-    $http.get("/mocks/estimations/" + $scope.shared.chosen_estimation + ".json")
-      .then(function (result) {
-        // all get succeeded
-        child_scope.estimation = result.data;
+    var child_scope = $scope.$new(),
+      promise1 = Model.estimateQuery($scope.shared.chosen_estimation, {
+        request: { plot: "linearRegression" },
+        covariables: $scope.query.coVariables.map(_map),
+        variables: $scope.query.variables.map(_map),
+        filters: $scope.query.filters.map(_map),
+        grouping: $scope.query.groupings.map(_map)
+      }),
+      promise2 = $http.get("/mocks/estimations/" + $scope.shared.chosen_estimation + ".json");
+
+    promise1.then(function (result) {
+      child_scope.result = result.data;
+      child_scope.help_is_open = false;
+    });
+
+    promise2.then(function (result) {
+      child_scope.estimation = result.data;
+    });
+
+    $q.all([promise1, promise2]).then(function () {
         $scope.loading_estimation = false;
-
-        child_scope.run_estimation = function () {
-
-          function _map (variable) {
-            return { "code": variable.code };
-          }
-
-          Model.estimateQuery(
-            $scope.shared.chosen_estimation,
-            {
-              request: { plot: "linearRegression" },
-              covariables: $scope.query.coVariables.map(_map),
-              variables: $scope.query.variables.map(_map),
-              filters: $scope.query.filters.map(_map),
-              grouping: $scope.query.groupings.map(_map)
-            }
-          ).then(function (result) {
-              child_scope.result = result.data;
-              child_scope.help_is_open = false;
-            });
-        };
+        child_scope.help_is_open = false;
 
         $modal.open({
           size: "lg",
@@ -74,7 +63,7 @@ angular.module('chuvApp.models').controller('EstimationController',['$scope', '$
 
         $modal.open({
           size: "md",
-          template: "<h1>Sorry!</h1> <h2>There was an error fetching the results</h2>"
+          template: "<div class='modal-header'><h1>Sorry!</h1></div><div class='modal-body'> <h2>There was an error fetching the results</h2></div>"
         });
       }
     );
