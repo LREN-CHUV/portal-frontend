f7d25921c9c4ca69f6a37cd05e82215543fd8d46
Ludovic Claude <ludovic.claude54@gmail.com>
2016-08-23 16:52:58 +0200

    Fix dockerize templates

--- docker/run.sh
+++ docker/run.sh
@@ -7,7 +7,7 @@ if [ ! -z "$PORTAL_BACKEND_SERVER" ]; then
   OPTS="$OPTS -wait tcp://${PORTAL_BACKEND_SERVER} -template /portal/conf/portal-backend-upstream.conf.tmpl:/etc/nginx/conf.d/portal-backend-upstream.conf"
   OPTS="$OPTS -template /portal/conf/proxy.conf.tmpl:/etc/nginx/conf.d/proxy.conf"
 fi
-if [ "$USE_GZIP" eq "true" ]; then
+if [ "$USE_GZIP" = "true" ]; then
   OPTS="$OPTS -template /portal/conf/gzip.conf.tmpl:/etc/nginx/conf.d/gzip.conf"
 else
   rm -f /etc/nginx/conf.d/gzip.conf
