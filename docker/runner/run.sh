09a50f079a10341c593c194fc5d08046d466fac7
Ludovic Claude <ludovic.claude54@gmail.com>
2016-08-26 18:19:25 +0200

    Increase wait for backend timeout

--- docker/runner/run.sh
+++ docker/runner/run.sh
@@ -4,7 +4,7 @@ export USE_GZIP=${USE_GZIP:-false}
 OPTS="-template /portal/conf/nginx.conf.tmpl:/etc/nginx/nginx.conf"
 
 if [ ! -z "$PORTAL_BACKEND_SERVER" ]; then
-  OPTS="$OPTS -wait tcp://${PORTAL_BACKEND_SERVER} -template /portal/conf/portal-backend-upstream.conf.tmpl:/etc/nginx/conf.d/portal-backend-upstream.conf"
+  OPTS="$OPTS -wait tcp://${PORTAL_BACKEND_SERVER} -timeout 100s -template /portal/conf/portal-backend-upstream.conf.tmpl:/etc/nginx/conf.d/portal-backend-upstream.conf"
   OPTS="$OPTS -template /portal/conf/proxy.conf.tmpl:/etc/nginx/conf.d/proxy.conf"
 fi
 if [ "$USE_GZIP" = "true" ]; then
