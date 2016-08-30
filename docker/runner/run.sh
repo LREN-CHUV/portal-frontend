8e175e633471721e36e4c079e65b4bbd10c6813c
Ludovic Claude <ludovic.claude54@gmail.com>
2016-08-30 19:34:05 +0200

    Wait for multiple backends

--- docker/runner/run.sh
+++ docker/runner/run.sh
@@ -4,7 +4,11 @@ export USE_GZIP=${USE_GZIP:-false}
 OPTS="-template /portal/conf/nginx.conf.tmpl:/etc/nginx/nginx.conf"
 
 if [ ! -z "$PORTAL_BACKEND_SERVER" ]; then
-  OPTS="$OPTS -wait tcp://${PORTAL_BACKEND_SERVER} -timeout 100s -template /portal/conf/portal-backend-upstream.conf.tmpl:/etc/nginx/conf.d/portal-backend-upstream.conf"
+  OPTS="$OPTS -timeout 100s -template /portal/conf/portal-backend-upstream.conf.tmpl:/etc/nginx/conf.d/portal-backend-upstream.conf"
+  IFS=','
+  for backend in ${PORTAL_BACKEND_SERVER} ; do
+    OPTS="$OPTS -wait tcp://$backend"
+  done
   OPTS="$OPTS -template /portal/conf/proxy.conf.tmpl:/etc/nginx/conf.d/proxy.conf"
 fi
 if [ "$USE_GZIP" = "true" ]; then
