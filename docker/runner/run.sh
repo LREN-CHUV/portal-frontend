77e5b90d002fed0d2d91635c0946932894335287
Ludovic Claude <ludovic.claude54@gmail.com>
2016-08-30 20:12:16 +0200

    Fix wait for multiple backends

--- docker/runner/run.sh
+++ docker/runner/run.sh
@@ -4,10 +4,10 @@ export USE_GZIP=${USE_GZIP:-false}
 OPTS="-template /portal/conf/nginx.conf.tmpl:/etc/nginx/nginx.conf"
 
 if [ ! -z "$PORTAL_BACKEND_SERVER" ]; then
-  OPTS="$OPTS -timeout 100s -template /portal/conf/portal-backend-upstream.conf.tmpl:/etc/nginx/conf.d/portal-backend-upstream.conf"
+  OPTS="$OPTS -template /portal/conf/portal-backend-upstream.conf.tmpl:/etc/nginx/conf.d/portal-backend-upstream.conf"
   IFS=','
   for backend in ${PORTAL_BACKEND_SERVER} ; do
-    OPTS="$OPTS -wait tcp://$backend"
+    OPTS="$OPTS -wait tcp://$backend -timeout 100s"
   done
   OPTS="$OPTS -template /portal/conf/proxy.conf.tmpl:/etc/nginx/conf.d/proxy.conf"
 fi
@@ -19,4 +19,4 @@ fi
 
 rm -f /etc/nginx/conf.d/default.conf
 
-exec dockerize $OPTS nginx -g 'daemon off;'
+eval "exec dockerize $OPTS nginx -g 'daemon off;'"
