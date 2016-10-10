8b32a3031d681f833935b6bdd6f804171d669800
Mirco Nasuti <mirco.nasuti@chuv.ch>
2016-10-10 15:46:07 +0200

    Revert "disable tSNE algo"
    This reverts commit af2ddaf9bc88c2cd4e733a622ef8a88d5825f422.

--- app/scripts/app/experiments/experiments.controller.js
+++ app/scripts/app/experiments/experiments.controller.js
@@ -44,12 +44,6 @@ angular.module('chuvApp.experiments')
             if (exareme.length > 0) {
               exareme[0].disable = true;
             }
-
-            var tSNE = $scope.ml_methods.filter(function(m){return m.code === "tSNE"});
-            if (tSNE.length > 0) {
-              tSNE[0].disable = true;
-            }
-
           }
         );
 
