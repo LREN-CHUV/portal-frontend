038fff51c32060f66769413488625f8c1230f919
Panbaba9n <goldgraalar@gmail.com>
2018-01-16 14:36:32 +0200

    run.sh tested

--- run.sh
+++ run.sh
@@ -23,3 +23,4 @@ if [ $no_cache ] ; then
 else
     docker run -v $(pwd)/app:/frontend/app -it --rm -p8000:8000 --name portal_frontend_dev hbpmip/portal-frontend-dev
 fi
+docker run -it --rm -p8000:8000 --name portal_frontend_dev hbpmip/portal-frontend-dev
