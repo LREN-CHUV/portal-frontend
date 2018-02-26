8854847d3ed77866919fc3bb8a2bf8892dba1872
Manuel Spuhler <manuel.spuhler@chuv.ch>
2018-02-26 18:18:05 +0100

    IA Model variables - no link

--- app/scripts/app/models/review/dataset.controller.js
+++ app/scripts/app/models/review/dataset.controller.js
@@ -229,7 +229,7 @@ angular.module("chuvApp.models").controller("DatasetController", [
       (_.isUndefined($scope.query.variables[0])
         ? null
         : $scope.query.variables[0]);
-
+    getDependantVariable;
     // Charts ressources
     var getHistogram = function(variable) {
       return variable ? Variable.get_histo(variable.code) : null;
