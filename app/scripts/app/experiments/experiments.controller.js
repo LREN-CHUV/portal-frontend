37241765f0f03470b4be69bd9ab06b4af9b8f8a2
Manuel Spuhler <manuel.spuhler@chuv.ch>
2018-03-01 14:04:15 +0100

    queries fix in url

--- app/scripts/app/experiments/experiments.controller.js
+++ app/scripts/app/experiments/experiments.controller.js
@@ -209,18 +209,12 @@ angular
             : [];
         };
 
-        const decodeFilters = () => {
-          return search.filterQuery ? JSON.parse(search.filterQuery) : "";
-        };
-
         $scope.query = {
           variables: map_query("variables"),
           groupings: map_query("groupings"),
           coVariables: map_query("coVariables"),
           filters: map_query("filters"),
-          filterQuery: !_.isEmpty(search.filterQuery)
-            ? JSON.parse(search.filterQuery)
-            : "",
+          filterQuery: !_.isEmpty(search.filterQuery) ? search.filterQuery : "",
           trainingDatasets: map_query("trainingDatasets")
         };
 
@@ -266,13 +260,8 @@ angular
                     .filter(k => validation[k])
                     .map(t => ({ code: t }));
 
-                  const filterQuery = !_.isEmpty(search.filterQuery)
-                    ? JSON.stringify($scope.query.filterQuery)
-                    : null;
-                  query.filterQuery = filterQuery;
-                  query.filters = $scope.query.filters
-                    .map(f => f.code)
-                    .join(",");
+                  query.filters = $scope.query.filterQuery;
+                  delete query.filterQuery;
 
                   $scope.model = {
                     title: child_scope.name,
