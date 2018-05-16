364a8c12e718491015d1df40790d4d15a576c425
Panbaba9n <goldgraalar@gmail.com>
2018-05-16 13:16:55 +0300

    add available_methods panel on ee page

--- app/scripts/app/models/variable_exploration/available_methods.component.js
+++ app/scripts/app/models/variable_exploration/available_methods.component.js
@@ -1,8 +1,7 @@
 "use strict";
 angular.module("chuvApp.models").component("availableMethods", {
   bindings: {
-    getFocusedVariable: "<",
-    zoom: "="
+    getFocusedVariable: "<"
   },
   require: {},
   controllerAs: "avmethods",
@@ -14,80 +13,35 @@ angular.module("chuvApp.models").component("availableMethods", {
     function($scope, Variable, MLUtils, Config) {
       var vm = this;
 
-      var ml_all_methods = [];
-
       vm.ml_methods = [];
-      vm.$onChanges = () => {
-        // Get all the ml methods
-        Config.then(config => config.mode === "local")
-          .then(isLocal => {
-            this.isLocal = isLocal;
-          })
-          .then(() => MLUtils.list_ml_methods())
-          .then(function(data) {
-            // FIXME Quick and dirty fix!
-            if (!this.isLocal) {
-              ml_all_methods = data.filter(function(m) {
-                return m.environment !== "Exareme";
-              });
-            }
-          });
 
-        // TODO: just filters by local methods
-        vm.ml_methods = true
-          ? ml_all_methods.filter(function(m) {
-              return m.code.substr(0, 3) !== "WP_";
-            })
-          : ml_all_methods.filter(function(m) {
-              return m.code.substr(0, 3) === "WP_";
-            });
+      // save config as local
+      Config.then(function(config) {
+        vm.config = config;
+      });
 
+      // Get the ml methods
+      MLUtils.list_ml_methods().then(function(data) {
+        vm.ml_methods = data
+          .filter(
+            f => ( vm.config.mode === "federation" ? true : f.environment !== "Exareme")
+          )
+          .filter(
+            f => f.code !== "histograms" && f.code !== "statisticsSummary"
+          );
+      });
+
+      vm.$onChanges = (data) => {
         vm.ml_methods.forEach(function(method) {
           method.available = available_method(method);
+          method.experimental = method.maturity === "experimental";
+          method.nyi = method.maturity === "coming_soon";
         });
       };
 
-      // Check if the method can be applied to the model
+      // Check if the method is not disabled
       function available_method(method) {
-        if (method.disable) {
-          return false;
-        }
-
-        // Check constraints
-        if (method.constraints) {
-          // Output constraints
-          if (method.constraints.variable) {
-            if (!method.constraints.variable.real) {
-              return false;
-            }
-          }
-
-          if (method.constraints.covariables) {
-            if (method.constraints.covariables.min_count) {
-              return false;
-            }
-
-            if (method.constraints.covariables.max_count) {
-              return false;
-            }
-          }
-
-          // Grouping constraints
-          if (method.constraints.grouping) {
-            if (method.constraints.grouping.min_count) {
-              return false;
-            }
-
-            if (method.constraints.grouping.max_count) {
-              return false;
-            }
-          }
-          if (!method.constraints.mixed) {
-            return false;
-          }
-        }
-
-        return true;
+        return (method.disable) ? false : true;
       }
     }
   ],
