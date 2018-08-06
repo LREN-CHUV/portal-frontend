4583696b48766e3c1acdec85023af9e5de22709f
Manuel Spuhler <manuel.spuhler@chuv.ch>
2018-08-06 10:01:43 +0200

    react-router + params parsing

--- app/v3/src/containers/App/App.tsx
+++ app/v3/src/containers/App/App.tsx
@@ -1,18 +1,29 @@
 import * as React from "react";
+import { BrowserRouter as Router, Route } from "react-router-dom";
 import { Provider } from "unstated";
 import { Experiment, Navigation } from "../../components/";
+// import { ExperimentContainer } from "../";
 
 import "./App.css";
 
+// const acomponent: React.Component = (
+//   <Subscribe to={[ExperimentContainer]}>
+//     {(experimentContainer: ExperimentContainer) => (
+//       <Experiment experimentStore={experimentContainer}/>
+//     )}
+//   </Subscribe>
+// );
 
 class App extends React.Component {
   public render() {
     return (
       <Provider>
-        <div className="App">
-          <Navigation />
-          <Experiment />
-        </div>
+        <Router>
+          <div className="App">
+            <Navigation />
+            <Route path="/experiment/:slug/:uuid" component={ Experiment } />
+          </div>
+        </Router>
       </Provider>
     );
   }
