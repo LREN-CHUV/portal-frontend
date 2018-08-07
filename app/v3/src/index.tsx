e48b09d1039c6c40324c243400b548371596bd05
Manuel Spuhler <manuel.spuhler@chuv.ch>
2018-08-07 16:57:29 +0200

    nginx setup wip

--- app/v3/src/index.tsx
+++ app/v3/src/index.tsx
@@ -2,10 +2,10 @@ import * as React from "react";
 import * as ReactDOM from "react-dom";
 import { App } from "./containers";
 import "./index.css";
-import registerServiceWorker from "./registerServiceWorker";
+import { unregister } from "./registerServiceWorker";
 
 import "bootstrap/dist/css/bootstrap-theme.css";
 import "bootstrap/dist/css/bootstrap.css";
 
 ReactDOM.render(<App />, document.getElementById("root") as HTMLElement);
-registerServiceWorker();
+unregister();
