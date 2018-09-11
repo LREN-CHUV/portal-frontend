d9c5721d9623a4f2277b7e3853a48e6e25c7c8ea
Manuel Spuhler <manuel.spuhler@chuv.ch>
2018-09-11 13:34:08 +0200

    Converted dict to enum

--- app/v3/src/constants.tsx
+++ app/v3/src/constants.tsx
@@ -1,19 +1,12 @@
 // Mime types
-const ERROR = "text/plain+error";
-const HIGHCHARTS = "application/vnd.highcharts+json";
-const JSON = "application/json";
-const MIP_PFA = "application/vnd.hbp.mip.experiment.pfa+json";
-const PFA = "application/pfa+json";
-const PLOTLY = "application/vnd.plotly.v1+json";
-
-export const MIME_TYPES = {
-  ERROR,
-  HIGHCHARTS,
-  JSON,
-  MIP_PFA,
-  PFA,
-  PLOTLY
-};
+export const enum MIME_TYPES {
+  ERROR = "text/plain+error",
+  HIGHCHARTS= "application/vnd.highcharts+json",
+  JSON= "application/json",
+  MIP_PFA = "application/vnd.hbp.mip.experiment.pfa+json",
+  PFA = "application/pfa+json",
+  PLOTLY = "application/vnd.plotly.v1+json"
+}
 
 interface ILabel {
   code: string;
