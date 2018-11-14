9d0eabe2bd628010f3b1c6286d99c2e88b0abb70
Manuel Spuhler <manuel.spuhler@chuv.ch>
2018-11-14 15:58:18 +0100

    wip

---
+++ app/v3/src/components/experiment/create/ExperimentCreateHeader.tsx
@@ -0,0 +1,94 @@
+// tslint:disable:no-console
+import { IExperimentResult, IModelResult } from "@app/types";
+import { ExperimentContainer, ModelContainer } from "../../../containers";
+
+import * as React from "react";
+import { Button, FormControl, Panel } from "react-bootstrap";
+import { RouteComponentProps, withRouter } from "react-router-dom";
+import { Dropdown } from "../..";
+import DropdownModel from "./DropdownModel";
+
+interface IProps extends RouteComponentProps<any> {
+  handleSaveAndRunExperiment: any;
+  handleChangeExperimentName: any;
+  selectedMethod: any;
+  experimentName: string;
+  experimentContainer: ExperimentContainer;
+  modelContainer: ModelContainer;
+}
+
+export default withRouter(
+  ({
+    handleSaveAndRunExperiment,
+    handleChangeExperimentName,
+    selectedMethod,
+    experimentName,
+    experimentContainer,
+    modelContainer,
+    history
+  }: IProps) => {
+    const state = modelContainer.state;
+    const title = (state && state.model && state.model.title) || "";
+    const experiments = experimentContainer.state.experiments;
+
+    const handleSelectExperiment = async (
+      selectedExperiment: IExperimentResult
+    ) => {
+      const { modelDefinitionId, uuid } = selectedExperiment;
+      history.push(`/v3/experiment/${modelDefinitionId}/${uuid}`);
+      const load = experimentContainer && experimentContainer.one;
+      return await load(uuid);
+    };
+
+    const handleSelectModel = async (selectedModel: IModelResult) => {
+      console.log(selectedModel);
+      const { slug } = selectedModel;
+      history.push(`/v3/experiment/${slug}`);
+      const load = modelContainer && modelContainer.one;
+      return await load(slug);
+    };
+
+    return (
+      <Panel className="experiment-header">
+        <Panel.Body>
+          <h3>
+            Create Experiment on{" "}
+            <DropdownModel
+              items={modelContainer.state.models}
+              title={title}
+              handleSelect={handleSelectModel}
+            />
+          </h3>
+          <div className="create-experiment-container">
+            <div className="item">
+              <FormControl
+                className="item experiment-name"
+                type="text"
+                placeholder={"Experiment name"}
+                value={experimentName}
+                onChange={handleChangeExperimentName}
+              />
+            </div>
+            <div className="item">
+              <Button
+                onClick={handleSaveAndRunExperiment}
+                bsStyle="info"
+                disabled={selectedMethod === undefined}
+              >
+                Run Experiment
+              </Button>
+            </div>
+            <div className="item">
+              <Dropdown
+                items={experiments}
+                title="RELATED EXPERIMENTS"
+                handleSelect={handleSelectExperiment}
+                handleCreateNewExperiment={null}
+              />
+            </div>
+          </div>
+        </Panel.Body>
+      </Panel>
+    );
+  }
+);
---
+++ app/v3/src/components/experiment/result/formats/PFA.tsx
@@ -0,0 +1,132 @@
+// tslint:disable:no-console
+
+import { SCORES } from "@app/constants";
+import {
+  IConfusionMatrix,
+  IKfoldValidationScore,
+  IMethod,
+  IPolynomialClassificationScore,
+  IValidationScore
+} from "@app/types";
+import { round } from "@app/utils";
+import * as React from "react";
+import { Tab, Tabs } from "react-bootstrap";
+import { Highchart } from "./";
+
+import "./JSON.css";
+import "./PFA.css";
+
+const removeKeys = (
+  obj: any,
+  keys: string[] = ["confusionMatrix", "type", "node"]
+) => {
+  const newObj = { ...obj };
+  keys.forEach(key => {
+    newObj[key] = undefined;
+  });
+
+  return JSON.parse(JSON.stringify(newObj));
+};
+
+const buildChart = (
+  validation:
+    | IKfoldValidationScore
+    | IValidationScore
+    | IPolynomialClassificationScore
+) => ({
+  chart: {
+    type: "column"
+  },
+  series: [
+    {
+      data: Object.keys(validation).map(key => validation[key]),
+      name: "Scores"
+    }
+  ],
+  title: {
+    text: ""
+  },
+  xAxis: {
+    categories: Object.keys(validation).map(v => SCORES[v] && SCORES[v].label)
+  },
+  yAxis: { title: { text: "Value" } }
+});
+
+const buildTableValue = (
+  validation:
+    | IKfoldValidationScore
+    | IValidationScore
+    | IPolynomialClassificationScore
+) =>
+  (validation && (
+    <ul className="pfa-table">
+      {Object.keys(validation).map((key, k) => (
+        <li key={k}>
+          <strong>{SCORES[key] && SCORES[key].label}</strong>:{" "}
+          {round(validation[key])}
+        </li>
+      ))}
+    </ul>
+  )) ||
+  null;
+
+const buildConfusionMatrix = (matrix: IConfusionMatrix) =>
+  matrix &&
+  ((
+    <table className="greyGridTable">
+      <caption>
+        <strong>Confusion matrix</strong>
+      </caption>
+      <thead>
+        <tr>
+          <th />
+          {matrix.labels.map((label: string, k: number) => (
+            <th key={k}>{label}*</th>
+          ))}
+        </tr>
+      </thead>
+      <tbody>
+        {matrix.values.map((values: number[], k: number) => (
+          <tr key={k}>
+            <th>{matrix.labels[k]}</th>
+            {values.map((value, l) => (
+              <td key={l}>{value}</td>
+            ))}
+          </tr>
+        ))}
+      </tbody>
+    </table>
+  ) ||
+    null);
+export default ({ method, data }: { method: IMethod; data: any }) => {
+  return (
+    (data && (
+      <Tabs defaultActiveKey={0} id="pfa-method" style={{ marginTop: "16px" }}>
+        {data && data.error && (
+          <div>
+            <h3>An error has occured</h3>
+            <p>{data.error}</p>
+          </div>
+        )}
+
+        {data.crossValidation && (
+          <Tab eventKey={0} title={"Cross Validation"}>
+            <Highchart options={buildChart(removeKeys(data.crossValidation))} />
+            {buildTableValue(removeKeys(data.crossValidation))}
+            {buildConfusionMatrix(data.crossValidation.confusionMatrix)}
+          </Tab>
+        )}
+        {data.remoteValidation && (
+          <Tab eventKey={1} title={"Remote Validation"}>
+            <Highchart
+              options={buildChart(removeKeys(data.remoteValidation))}
+            />
+            {buildTableValue(removeKeys(data.remoteValidation))}
+            {buildConfusionMatrix(data.remoteValidation.confusionMatrix)}
+          </Tab>
+        )}
+      </Tabs>
+    )) ||
+    null
+  );
+};
