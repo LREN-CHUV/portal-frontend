e7430ecd02335afbd77c33af5d162e4dcc3ef6fd
Manuel Spuhler <manuel.spuhler@chuv.ch>
2018-12-13 13:21:05 +0100

    Mining data

--- app/v3/src/components/Experiment/Review/Table.tsx
+++ app/v3/src/components/Experiment/Review/Table.tsx
@@ -1,43 +1,92 @@
+import { round } from "@app/utils";
+import { Column } from "primereact/column";
+import { DataTable } from "primereact/datatable";
 import * as React from "react";
-import BootstrapTable from "react-bootstrap-table-next";
+
+import "primeicons/primeicons.css";
+import "primereact/resources/primereact.min.css";
+import "primereact/resources/themes/nova-light/theme.css";
+
 interface IProps {
   mining?: any[];
 }
 
-const Table = ({ mining }: IProps) => {
+const Table = ({ mining: datasets }: IProps) => {
+  if (!datasets) {
+    return null;
+  }
 
-  if (!mining) {return null}
+  const datasetDatas = datasets.map(
+    dataset =>
+      (dataset.data &&
+        dataset.data.data &&
+        dataset.data.data.length &&
+        dataset.data.data.filter(
+          (r: any) => r.group && r.group[0] === "all"
+        )) ||
+      {}
+  );
+
+  // get variables indexes
+  const indexes =
+    datasetDatas.length && datasetDatas[0].map((d: any) => d.index);
+
+  // populate each variable data by row
+  const rows: any[] = [];
+  indexes.forEach((index: any) => {
+    const row: any = {};
+    datasetDatas.map((datasetData: any, i: number) => {
+      const dataRow = datasetData.find((d: any) => d.index === index);
+      row[i] = dataRow;
+    });
+    rows.push(row);
+  });
+
+  // compute rows data for output
+  const computedRows: any[] = [];
+  rows.map((row: any) => {
+    const computedRow: any = {};
+    const polynominalRows: any[] = [];
+    let polynominalRow: any;
+
+    console.log(row)
+    Object.keys(row).map((rowKey: any) => {
+      const col = row[rowKey];
+      computedRow.variable = col.label;
+
+      if (col.frequency) {
+        computedRow[rowKey] = row[rowKey].count;
+        Object.keys(col.frequency).map((k: any) => {
+          polynominalRow = polynominalRows.find(p => p.variable === k);
+          if (!polynominalRow) {
+            polynominalRow = {}
+            polynominalRows.push(polynominalRow);
+          }
+          polynominalRow[rowKey] = col.frequency[k];
+          polynominalRow.variable = k;
+        });
+      } else {
+        const mean = round(row[rowKey].mean, 2);
+        const min = round(row[rowKey].min, 2);;
+        const max = round(row[rowKey].max, 2);;
+        const std = round(row[rowKey].std, 2);;
+        computedRow[rowKey] = mean ? `${mean} (${min}-${max}) - std: ${std}` : '-';
+      }
+    });
+
+    computedRows.push(computedRow);
+    polynominalRows.map((p: any) => {
+      computedRows.push(p);
+    });
+  });
 
   const columns = [
-    {
-      dataField: "label",
-      text: "Variables"
-    },
-    ...mining.map((dataset: any) => ({
-      dataField: "std",
-      text: dataset.name
-    }))
+    <Column header="variable" field="variable" key={"variable"} />,
+    ...datasets.map((dataset: any, index: number) => (
+      <Column header={dataset.name} field={`${index}`} key={index} />
+    ))
   ];
 
-  // const products = [
-  //   { id: 12, name: "Item 12", price: 12.5, inStock: false },
-  //   { id: 13, name: "Item 13", price: 13.5, inStock: true },
-  //   { id: 14, name: "Item 14", price: 14.5, inStock: true }
-  // ];
-
-  const datasets =
-    mining.map((m: any, i: number) =>
-      m.data.data
-        .filter((r: any) => r.group && r.group[0] === "all")
-    );
-  console.log(columns, datasets);
-
-  return (
-    <div>
-      {datasets && (
-        <BootstrapTable keyField="label" data={datasets} columns={columns} />
-      )}
-    </div>
-  );
+  return <DataTable value={computedRows}>{columns}</DataTable>;
 };
 export default Table;
