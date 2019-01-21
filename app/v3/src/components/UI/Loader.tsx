64f4d2cffaeb531e75f9027459a1916081ddff8e
Manuel Spuhler <manuel.spuhler@chuv.ch>
2019-01-21 13:27:27 +0100

    Loding state

--- app/v3/src/components/UI/Loader.tsx
+++ app/v3/src/components/UI/Loader.tsx
@@ -1,18 +1,19 @@
 import React from "react";
 import { RingLoader } from "react-spinners";
 
+import "./Loader.css";
 
 class Loader extends React.Component {
-
   public render() {
     return (
-      <div>
+      <div className="loader">
         <RingLoader
           sizeUnit={"px"}
           size={16}
           color={"#0c6c94"}
           loading={true}
         />
+        <p>loading data...</p>
       </div>
     );
   }
