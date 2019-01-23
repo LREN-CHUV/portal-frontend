5ab1099f73565c59c6071ddf88d09b1c5be6181f
Manuel Spuhler <manuel.spuhler@chuv.ch>
2019-01-23 15:34:02 +0100

    Bad url

--- app/v3/src/components/API/URLS.tsx
+++ app/v3/src/components/API/URLS.tsx
@@ -1,4 +1,4 @@
 const URL = `${location.protocol}//${location.host}`;
 
 export const webURL = URL;
-export const backendURL = `http://frontend/services`;
+export const backendURL = `${URL}/services`;
