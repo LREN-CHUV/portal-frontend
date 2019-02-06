c0be2b8bb13e6dbeba513f5dc7766867a4f3acbc
Manuel Spuhler <manuel.spuhler@gmail.com>
2019-02-07 00:06:24 +0100

    Table achieved

--- app/v3/src/components/Experiment/Review/Table.tsx
+++ app/v3/src/components/Experiment/Review/Table.tsx
@@ -24,113 +24,14 @@ interface IComputeMiningResult {
 
 interface ITableRow {
   variable?: string;
-  category?: string;
+  category?: MIP.API.IVariable;
 }
 
-const computeMiningResultToTable = ({
-  minings,
-  selectedDatasets,
-  variables
-}: IComputeMiningResult): ITableRow[] => {
-  const computedRows: ITableRow[] = [];
-
-  if (!minings || !selectedDatasets || !variables) {
-    return computedRows;
-  }
-
-  const processData = (code: string, data: any) => {
-    const theData = data.data;
-    let value = "data";
-    const polynominalRows: any[] = [];
-
-    if (theData) {
-      const variableData = theData.find(
-        (r: any) => r.group && (r.group[0] === "all" && r.index === code)
-      );
-
-      if (variableData) {
-        if (variableData.frequency) {
-          value = `${variableData.count} (null: ${variableData.null_count})`;
-          Object.keys(variableData.frequency).map((k: any) => {
-            polynominalRows.push(`${variableData.frequency[k]}`);
-            // category = k
-          });
-        } else {
-          const mean = round(variableData.mean, 2);
-          const min = round(variableData.min, 2);
-          const max = round(variableData.max, 2);
-          const std = round(variableData.std, 2);
-          value = mean ? `${mean} (${min}-${max}) - std: ${std}` : "null";
-        }
-      }
-    }
-
-    return value;
-  };
-
-  const rows: ITableRow[] = [];
-  variables.forEach(variable => {
-    const isPolynominal =
-      variable.type === "polynominal" && variable.enumerations;
-    selectedDatasets.map(dataset => {
-      const row: ITableRow = {};
-      let polynominalRows: ITableRow[] = [];
-
-      // set labels, Variables + polynominal categories rows
-      if (isPolynominal) {
-        row.variable = variable.label;
-        // tslint:disable
-        variable.enumerations &&
-          variable.enumerations.forEach(e => {
-            polynominalRows.push({
-              category: e.label
-            });
-          });
-        // tslint:enable
-      } else {
-        row.variable = variable.label;
-      }
-
-      // Process data
-      const code = dataset.code;
-      const mining = minings.find((m: any) => m.dataset.code === code);
-      if (mining) {
-        const { error, data } = mining;
-        if (!error && !data) {
-          row[code] = "loading...";
-          if (isPolynominal) {
-            polynominalRows = polynominalRows.map(r => ({ ...r, [code]: "loading..."}))
-          } 
-        } else if (error) {
-          row[code] = "error";
-          if (isPolynominal) {
-            polynominalRows = polynominalRows.map(r => ({ ...r, [code]: "error"}))
-          } 
-        } else if (data) {
-          const value = processData(variable.code, data);
-          row[code] = value;
-        }
-      }
-      rows.push(row);
-      polynominalRows.forEach(r => rows.push(r));
-    });
-  });
-
-  return rows;
-};
-
-const variableTemplate = (rowData: any, column: any) => (
-  <span
-    className={rowData.category ? "datatable category" : "datatable variable"}
-  >
-    {rowData.category ? rowData.category : rowData.variable}
-  </span>
-);
-
 const Table = ({ minings, selectedDatasets, query, lookup }: IProps) => {
-  const rows = computeMiningResultToTable({
+  const rows = computeMinings({
     minings,
     selectedDatasets,
+    // FIXME: what about undefined ?
     variables:
       query &&
       query.variables &&
@@ -157,4 +58,141 @@ const Table = ({ minings, selectedDatasets, query, lookup }: IProps) => {
 
   return <DataTable value={rows}>{columns}</DataTable>;
 };
+
+const findVariableData = (code: string, data: any) => {
+  const theData = data.data;
+
+  if (theData) {
+    const variableData = theData.find(
+      (r: any) => r.group && (r.group[0] === "all" && r.index === code)
+    );
+
+    return variableData;
+  }
+
+  return;
+};
+
+const processPolynominalDataHeader = (code: string, data: any) => {
+  const variableData = findVariableData(code, data);
+  if (variableData) {
+    return `${variableData.count} (null: ${variableData.null_count})`;
+  }
+
+  return;
+};
+
+const processPolynominalData = (
+  code: string,
+  categoryCode: string,
+  data: any
+) => {
+  const variableData = findVariableData(code, data);
+  if (variableData) {
+    return variableData.frequency[categoryCode];
+  }
+
+  return;
+};
+
+const processContinuousData = (code: string, data: any) => {
+  const variableData = findVariableData(code, data);
+  if (variableData) {
+    const mean = round(variableData.mean, 2);
+    const min = round(variableData.min, 2);
+    const max = round(variableData.max, 2);
+    const std = round(variableData.std, 2);
+
+    return mean ? `${mean} (${min}-${max}) - std: ${std}` : "null";
+  }
+
+  return;
+};
+
+const computeMinings = ({
+  minings,
+  selectedDatasets,
+  variables
+}: IComputeMiningResult): ITableRow[] => {
+  const computedRows: ITableRow[] = [];
+
+  if (!minings || !selectedDatasets || !variables) {
+    return computedRows;
+  }
+
+  const rows: ITableRow[] = [];
+  variables.forEach(variable => {
+    const isPolynominal = variable.type === "polynominal";
+    const row: ITableRow = {};
+    let polynominalRows: ITableRow[] = [];
+
+    // set labels, Variables + polynominal categories rows
+    if (isPolynominal && variable.enumerations) {
+      row.variable = variable.label;
+      polynominalRows = variable.enumerations.reduce(
+        (acc, val) => [
+          ...acc,
+          { category: { code: val.code, label: val.label } }
+        ],
+        []
+      );
+    } else {
+      row.variable = variable.label;
+    }
+
+    selectedDatasets.map(dataset => {
+      // Process data
+      const code = dataset.code;
+      const mining = minings.find((m: any) => m.dataset.code === code);
+      if (mining) {
+        const { error, data } = mining;
+        if (!error && !data) {
+          row[code] = "loading...";
+          if (isPolynominal) {
+            polynominalRows = polynominalRows.map(r => ({
+              ...r,
+              [code]: "loading..."
+            }));
+          }
+        } else if (error) {
+          row[code] = "error";
+          if (isPolynominal) {
+            polynominalRows = polynominalRows.map(r => ({
+              ...r,
+              [code]: "error"
+            }));
+          }
+        } else if (data) {
+          if (isPolynominal) {
+            row[code] = processPolynominalDataHeader(variable.code, data);
+            polynominalRows = polynominalRows.map(r => ({
+              ...r,
+              [code]: processPolynominalData(
+                variable.code,
+                r.category!.code,
+                data
+              )
+            }));
+          } else {
+            row[code] = processContinuousData(variable.code, data);
+          }
+        }
+      }
+    });
+
+    rows.push(row);
+    polynominalRows.forEach(r => rows.push(r));
+  });
+
+  return rows;
+};
+
+const variableTemplate = (rowData: any, column: any) => (
+  <span
+    className={rowData.category ? "datatable category" : "datatable variable"}
+  >
+    {rowData.category ? rowData.category.label : rowData.variable}
+  </span>
+);
+
 export default Table;
