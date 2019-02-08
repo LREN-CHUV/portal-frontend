a95195cf6c18a0bdaf1232bd90d55cfdb1ef25a8
Manuel Spuhler <manuel.spuhler@chuv.ch>
2019-02-08 13:44:21 +0100

    Added Bugsnag client

--- app/v3/src/index.tsx
+++ app/v3/src/index.tsx
@@ -1,8 +1,19 @@
+import bugsnag from "@bugsnag/js";
+import bugsnagReact from "@bugsnag/plugin-react";
 import * as React from "react";
 import * as ReactDOM from "react-dom";
 import { default as AppContainer } from "./components/App/Container";
 import "./index.css";
 import { unregister } from "./registerServiceWorker";
 
-ReactDOM.render(<AppContainer />, document.getElementById("root") as HTMLElement);
+const bugsnagClient = bugsnag("87e28aed7927156bee7f8accd10ed20a");
+bugsnagClient.use(bugsnagReact, React);
+const ErrorBoundary = bugsnagClient.getPlugin("react");
+
+ReactDOM.render(
+  <ErrorBoundary>
+    <AppContainer />
+  </ErrorBoundary>,
+  document.getElementById("root") as HTMLElement
+);
 unregister();
