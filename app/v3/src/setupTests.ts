93727672054e11e7c75a1617f0f15aeb13352119
Manuel Spuhler <manuel.spuhler@chuv.ch>
2019-02-13 09:39:27 +0100

    Enzyme testing lib setup

--- app/v3/src/setupTests.ts
+++ app/v3/src/setupTests.ts
@@ -1,5 +1,10 @@
+import { configure } from "enzyme";
+import Adapter from "enzyme-adapter-react-16";
+
 function noOp() {}
 
 if (typeof window.URL.createObjectURL === "undefined") {
   Object.defineProperty(window.URL, "createObjectURL", { value: noOp });
 }
+
+configure({ adapter: new Adapter() });
