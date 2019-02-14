9d75e080f04bd84b87f87dff42594293d184fd39
Manuel Spuhler <manuel.spuhler@gmail.com>
2019-02-14 17:28:44 +0100

    naivebayes test

--- app/v3/src/components/Experiment/Result/__tests__/Algorithms.test.tsx
+++ app/v3/src/components/Experiment/Result/__tests__/Algorithms.test.tsx
@@ -6,22 +6,41 @@ import APIAdapter from "../../../API/APIAdapter";
 import { MIP } from "@app/types";
 
 const stringify = (json: any): void => {
-  console.log(JSON.stringify(json, null, 2))
-}
+  console.log(JSON.stringify(json, null, 2));
+};
 
-const parseExperiment = (json: any) : MIP.API.IExperimentResponse => APIAdapter.parse(json)
+const parseExperiment = (json: any): MIP.API.IExperimentResponse =>
+  APIAdapter.parse(json);
 
 describe("Test Experiment results", () => {
-  let props;
-
-  beforeEach(() => {
-    props = {
-      experimentState: {}
-    };
-  });
-
   it("Federated linearRegression algorithm renders correctly", () => {
     const response = require("../__mocks__/responses/fed-linearRegression.json");
+    const experiment = parseExperiment(response);
+    const props = {
+      experimentState: {
+        experiment
+      }
+    };
+    const wrapper = mount(<Result {...props} />);
+    expect(
+      wrapper
+        .find(".greyGridTable tbody tr td")
+        .first()
+        .text()
+    ).toEqual("rightphgparahippocampalgyrus");
+    expect(
+      wrapper
+        .find(".greyGridTable tbody tr td")
+        .at(1)
+        .text()
+    ).toEqual("5.227");
+    expect(wrapper.find(".error")).toHaveLength(0);
+    expect(wrapper.find(".loading")).toHaveLength(0);
+    expect(wrapper.find("div#tabs-methods")).toHaveLength(1);
+  });
+
+  it("Federated naiveBayes algorithm renders correctly", () => {
+    const response = require("../__mocks__/responses/fed-naivebayes.json");
     const experiment = parseExperiment(response);
     const props = {
       experimentState: {
@@ -32,7 +51,21 @@ describe("Test Experiment results", () => {
 
     expect(wrapper.find(".error")).toHaveLength(0);
     expect(wrapper.find(".loading")).toHaveLength(0);
-    expect(wrapper.find("div#tabs-methods")).toHaveLength(1);
+    expect(wrapper.find("div#tabs-node")).toHaveLength(1);
+    expect(wrapper.find("a#tabs-node-tab-0")).toHaveLength(1);
+    expect(wrapper.find("a#tabs-node-tab-1")).toHaveLength(1);
+    const table = wrapper.find("ul.pfa-table");
+    expect(table).toHaveLength(4);
+    expect(
+      table
+        .find("li")
+        .first()
+        .equals(
+          <li>
+            <strong>Accuracy</strong>: 0.457
+          </li>
+        )
+    ).toEqual(true);
+    expect(wrapper.find(".greyGridTable")).toHaveLength(4);
   });
-
 });
