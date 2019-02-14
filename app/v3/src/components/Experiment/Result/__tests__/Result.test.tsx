c4cfb4cf1244423a6aea6006896de9c1f92d371b
Manuel Spuhler <manuel.spuhler@gmail.com>
2019-02-14 11:58:25 +0100

    Testing federated linear regression algorithm visualisation

---
+++ app/v3/src/components/Experiment/Result/__tests__/Result.test.tsx
@@ -0,0 +1,74 @@
+import Result from "../Result";
+import * as React from "react";
+import renderer from "react-test-renderer";
+import { shallow, mount } from "enzyme";
+import APIAdapter from "../../../API/APIAdapter";
+
+const stringify = (json: any): void => {
+  console.log(JSON.stringify(json, null, 2))
+}
+
+describe("Test Experiment results", () => {
+  let props;
+
+  beforeEach(() => {
+    props = {
+      experimentState: {}
+    };
+  });
+
+  it("dom renders correctly", () => {
+    const wrapper = shallow(<Result {...props} />);
+    expect(wrapper).toBeDefined();
+  });
+
+  it("snapshot renders correctly", () => {
+    const tree = renderer.create(<Result {...props} />).toJSON();
+    expect(tree).toMatchSnapshot();
+  });
+
+  it("loading renders correctly", () => {
+    const wrapper = shallow(<Result {...props} />);
+    const loading = wrapper.find(".loading");
+    expect(loading).toHaveLength(1);
+  });
+
+  it("!loading renders correctly", () => {
+    const props = {
+      experimentState: {
+        experiment: {
+          results: []
+        }
+      }
+    };
+    const wrapper = shallow(<Result {...props} />);
+    const loading = wrapper.find(".loading");
+    expect(loading).toHaveLength(0);
+  });
+
+  it("error renders correctly", () => {
+    const props = {
+      experimentState: {
+        error: "ouch"
+      }
+    };
+    const wrapper = shallow(<Result {...props} />);
+    const error = wrapper.find(".error");
+    expect(error).toHaveLength(1);
+  });
+
+  it("Federated linearRegression algorithm renders correctly", () => {
+    const response = require("../__mocks__/responses/fed-linearRegression.json");
+    const experiment = APIAdapter.parse(response);
+    const props = {
+      experimentState: {
+        experiment
+      }
+    };
+    const wrapper = mount(<Result {...props} />);
+
+    expect(wrapper.find(".error")).toHaveLength(0);
+    expect(wrapper.find(".loading")).toHaveLength(0);
+    expect(wrapper.find("div#tabs-methods")).toHaveLength(1);
+  });
+});
