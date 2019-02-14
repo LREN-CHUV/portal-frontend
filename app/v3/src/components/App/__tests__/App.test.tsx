10171354d347361c51fd4520e95eb41395581fa6
Manuel Spuhler <manuel.spuhler@gmail.com>
2019-02-14 12:51:10 +0100

    Reworked existing tests for enzyme

--- app/v3/src/components/App/__tests__/App.test.tsx
+++ app/v3/src/components/App/__tests__/App.test.tsx
@@ -8,35 +8,50 @@ import config from "@app/components/API/RequestHeaders";
 import App from "@app/components/App/App";
 import AppContainer from "@app/components/App/Container";
 import * as React from "react";
-import * as ReactDOM from "react-dom";
 import { BrowserRouter as Router } from "react-router-dom";
+import { shallow } from "enzyme";
+import renderer from "react-test-renderer";
 
 jest.mock("request-promise-native");
 
-const apiExperiment = new APIExperiment(config);
-const apiModel = new APIModel(config);
-const apiCore = new APICore(config);
-const apiMining = new APIMining(config);
+describe("Test App component", () => {
+  const apiExperiment = new APIExperiment(config);
+  const apiModel = new APIModel(config);
+  const apiCore = new APICore(config);
+  const apiMining = new APIMining(config);
+  const props = {
+    appConfig: { appConfig: "MIP" },
+    apiExperiment,
+    apiCore,
+    apiModel,
+    apiMining
+  };
 
-it("renders without crashing", () => {
-  const div = document.createElement("div");
-  ReactDOM.render(<AppContainer />, div);
-  ReactDOM.unmountComponentAtNode(div);
-});
+  it("AppContainer dom renders correctly", () => {
+    const wrapper = shallow(<AppContainer />);
+    expect(wrapper).toBeDefined();
+  });
 
-it("renders without crashing", () => {
-  const div = document.createElement("div");
-  ReactDOM.render(
-    <Router>
-      <App
-        appConfig={{ appConfig: "MIP" }}
-        apiExperiment={apiExperiment}
-        apiCore={apiCore}
-        apiModel={apiModel}
-        apiMining={apiMining}
-      />
-    </Router>,
-    div
-  );
-  ReactDOM.unmountComponentAtNode(div);
+  it("AppContainer snapshot renders correctly", () => {
+    const tree = renderer.create(<AppContainer />).toJSON();
+    expect(tree).toMatchSnapshot();
+  });
+
+  it("App dom renders correctly", () => {
+    const wrapper = shallow(
+        <App {...props} />
+    );
+    expect(wrapper).toBeDefined();
+  });
+
+  it("App snapshot renders correctly", () => {
+    const tree = renderer
+      .create(
+        <Router>
+          <App {...props} />
+        </Router>
+      )
+      .toJSON();
+    expect(tree).toMatchSnapshot();
+  });
 });
---
+++ app/v3/src/components/Experiment/Create/__tests__/Create.test.tsx
@@ -0,0 +1,36 @@
+import { APICore, APIExperiment, APIModel } from "@app/components/API";
+import config from "@app/components/API/RequestHeaders";
+import ExperimentCreate from "@app/components/Experiment/Create/Container";
+import * as React from "react";
+import { BrowserRouter as Router } from "react-router-dom";
+import { shallow } from "enzyme";
+// import renderer from "react-test-renderer";
+
+describe("Test Create component", () => {
+  const apiExperiment = new APIExperiment(config);
+  const apiModel = new APIModel(config);
+  const apiCore = new APICore(config);
+  const props = {
+    apiExperiment,
+    apiCore,
+    apiModel
+  };
+
+  const component = (
+    <Router>
+      <ExperimentCreate {...props} />
+    </Router>
+  );
+
+  it("Create dom renders correctly", () => {
+    const wrapper = shallow(component);
+    expect(wrapper).toBeDefined();
+  });
+
+  // TODO: async testing
+  // https://github.com/airbnb/enzyme/issues/346
+  // it("Create snapshot renders correctly", () => {
+  //   const tree = renderer.create(component).toJSON();
+  //   expect(tree).toMatchSnapshot();
+  // });
+});
