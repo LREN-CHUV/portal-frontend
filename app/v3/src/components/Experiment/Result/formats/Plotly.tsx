6d7f6be7cea24e33c9fae870d1552a2cb4dc5852
Manuel Spuhler <manuel.spuhler@gmail.com>
2019-02-21 22:33:36 +0100

    Modularize plotly

--- app/v3/src/components/Experiment/Result/formats/Plotly.tsx
+++ app/v3/src/components/Experiment/Result/formats/Plotly.tsx
@@ -1,6 +1,8 @@
-import * as React from "react";
-import Plot from "react-plotly.js";
+import Plotly from 'plotly.js/dist/plotly-basic';
+import * as React from 'react';
+import createPlotlyComponent from 'react-plotlyjs';
+const PlotlyComponent = createPlotlyComponent(Plotly);
 
 export default ({ data, layout }: { data: any; layout: any }) => (
-  <Plot data={data} layout={layout} />
+  <PlotlyComponent data={data} layout={layout} />
 );
