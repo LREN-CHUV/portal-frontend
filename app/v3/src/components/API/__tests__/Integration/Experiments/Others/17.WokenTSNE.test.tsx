a652efcd1661a3fc6ad7298e4fe3c9f0706cc2d4
Manuel Spuhler <manuel.spuhler@chuv.ch>
2019-03-13 11:39:47 +0100

    Test for tSNE

---
+++ app/v3/src/components/API/__tests__/Integration/Experiments/Others/17.WokenTSNE.test.tsx
@@ -0,0 +1,86 @@
+import { mount } from 'enzyme';
+import Result from '../../../../../Experiment/Result/Result';
+import { MIP } from '../../../../../../types';
+import * as React from 'react';
+import {
+  datasets,
+  createExperiment,
+  createModel,
+  waitForResult
+} from '../../../../../utils/TestUtils';
+
+
+// config
+
+const modelSlug = `model-${Math.round(Math.random() * 10000)}`;
+const experimentCode = 'tSNE';
+const model: any = (datasets: MIP.API.IVariableEntity[]) => ({
+  query: {
+    coVariables: [{ code: 'lefthippocampus' }],
+    groupings: [],
+    testingDatasets: [],
+    filters:
+      '{"condition":"AND","rules":[{"id":"subjectageyears","field":"subjectageyears","type":"integer","input":"number","operator":"greater","value":"65"}],"valid":true}',
+    trainingDatasets: datasets.map(d => ({ code: d.code })),
+    validationDatasets: [],
+    variables: [{ code: 'subjectageyears' }]
+  }
+});
+
+const payload: MIP.API.IExperimentPayload = {
+  algorithms: [
+    {
+      code: experimentCode,
+      name: experimentCode,
+      parameters: [],
+      validation: false
+    }
+  ],
+  model: modelSlug,
+  name: `${experimentCode}-${modelSlug}`,
+  validations: []
+};
+
+describe('Integration Test for experiment API', () => {
+  beforeAll(async () => {
+    const dstate = await datasets();
+    expect(dstate.error).toBeFalsy();
+    expect(dstate.datasets).toBeTruthy();
+
+    if (dstate.datasets) {
+      const mstate = await createModel({
+        model: model(dstate.datasets),
+        modelSlug
+      });
+      expect(mstate.error).toBeFalsy();
+      expect(mstate.model).toBeTruthy();
+
+      return true;
+    }
+  });
+
+  // Test
+
+  it(`create ${experimentCode}`, async () => {
+    const { error, experiment } = await createExperiment({
+      experiment: payload
+    });
+    expect(error).toBeFalsy();
+    expect(experiment).toBeTruthy();
+
+    const uuid = experiment && experiment.uuid;
+    expect(uuid).toBeTruthy();
+    if (!uuid) {
+      throw new Error('uuid not defined');
+    }
+    
+    const experimentState = await waitForResult({ uuid });
+    expect(experimentState.error).toBeFalsy();
+    expect(experimentState.experiment).toBeTruthy();
+
+    const props = { experimentState };
+    const wrapper = mount(<Result {...props} />);
+    expect(wrapper.find('.error')).toHaveLength(0);
+    expect(wrapper.find('.loading')).toHaveLength(0);
+  });
+});
