0e382e15328f2290b9af88f90586ef011dc89611
Manuel Spuhler <manuel.spuhler@gmail.com>
2019-05-17 22:31:51 +0200

    Cleanup

--- app/v3/src/components/Model/Explore/Header.tsx
+++ app/v3/src/components/Model/Explore/Header.tsx
@@ -1,12 +1,15 @@
 import * as React from 'react';
 import { Button, Glyphicon, Panel } from 'react-bootstrap';
 
-export default () => {
+interface Props {
+  handleGoToAnalysis: any;
+}
+export default ({ handleGoToAnalysis }: Props) => {
   return (
     <Panel>
       <Panel.Body>
         {/* <h3>Epidemiological Exploration</h3> */}
-        <Button bsStyle='info' type='submit'>
+        <Button bsStyle='info' type='submit' onClick={handleGoToAnalysis}>
           Interactive Analysis <Glyphicon glyph='chevron-right' />
         </Button>
       </Panel.Body>
