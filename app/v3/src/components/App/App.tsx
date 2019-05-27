7fc39240f3c1b3c3496620cbde2526b5d19f18c3
Manuel Spuhler <manuel.spuhler@chuv.ch>
2019-05-27 14:32:46 +0200

    Home page infos

--- app/v3/src/components/App/App.tsx
+++ app/v3/src/components/App/App.tsx
@@ -1,7 +1,10 @@
 import 'bootstrap/dist/css/bootstrap.css';
+import './App.css';
+
 // tslint:disable jsx-no-lambda
 import * as React from 'react';
 import { Route } from 'react-router-dom';
+
 import { APICore, APIExperiment, APIMining, APIModel } from '../API';
 import ExperimentCreate from '../Create/Container';
 import Explore from '../Explore/Container';
@@ -9,7 +12,6 @@ import ExperimentResult from '../Result/Container';
 import ExperimentReview from '../Review/Container';
 import Footer from '../UI/Footer';
 import Navigation from '../UI/Navigation';
-import './App.css';
 
 interface Props {
   appConfig: any;
@@ -19,64 +21,30 @@ interface Props {
   apiMining: APIMining;
 }
 
-const App = ({
-  appConfig,
-  apiExperiment,
-  apiCore,
-  apiModel,
-  apiMining
-}: Props) => (
+const App = ({ appConfig, apiExperiment, apiCore, apiModel, apiMining }: Props) => (
   <div className='App'>
     <header>
-      <Navigation
-        apiExperiment={apiExperiment}
-        apiModel={apiModel}
-        appConfig={appConfig}
-      />
+      <Navigation apiExperiment={apiExperiment} apiModel={apiModel} appConfig={appConfig} />
     </header>
     <section className='main-content'>
+      <Route path='/v3/home' render={() => <Home />} />
       <Route
         path={['/v3/explore/:slug', '/v3/explore']}
-        render={(props) => (
-          <Explore
-            apiCore={apiCore}
-            apiMining={apiMining}
-            apiModel={apiModel}
-            {...props}
-          />
-        )}
+        render={props => <Explore apiCore={apiCore} apiMining={apiMining} apiModel={apiModel} {...props} />}
       />
       <Route
         path={['/v3/review/:slug', '/v3/review']}
-        render={(props) => (
-          <ExperimentReview
-            apiMining={apiMining}
-            apiModel={apiModel}
-            apiCore={apiCore}
-            {...props}
-          />
-        )}
+        render={props => <ExperimentReview apiMining={apiMining} apiModel={apiModel} apiCore={apiCore} {...props} />}
       />
       <Route
         path='/v3/experiment/:slug/:uuid'
-        render={() => (
-          <ExperimentResult
-            apiExperiment={apiExperiment}
-            apiModel={apiModel}
-            apiCore={apiCore}
-          />
-        )}
+        render={() => <ExperimentResult apiExperiment={apiExperiment} apiModel={apiModel} apiCore={apiCore} />}
       />
       <Route
         exact={true}
         path='/v3/experiment/:slug'
         render={() => (
-          <ExperimentCreate
-            apiExperiment={apiExperiment}
-            apiCore={apiCore}
-            apiModel={apiModel}
-            appConfig={appConfig}
-          />
+          <ExperimentCreate apiExperiment={apiExperiment} apiCore={apiCore} apiModel={apiModel} appConfig={appConfig} />
         )}
       />
     </section>
