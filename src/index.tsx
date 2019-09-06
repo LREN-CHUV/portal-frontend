387190617401b558986d93001aa281aaca079325
Manuel Spuhler <manuel.spuhler@chuv.ch>
2019-09-06 08:39:24 +0200

    Bugsnag

--- src/index.tsx
+++ src/index.tsx
@@ -1,19 +1,23 @@
-// import bugsnag from "@bugsnag/js";
-// import bugsnagReact from "@bugsnag/plugin-react";
-import * as React from "react";
-import * as ReactDOM from "react-dom";
-import { default as AppContainer } from "./components/App/Container";
-import "./index.css";
-import { unregister } from "./registerServiceWorker";
+import bugsnag from '@bugsnag/js';
+import bugsnagReact from '@bugsnag/plugin-react';
+import * as React from 'react';
+import * as ReactDOM from 'react-dom';
+import { default as AppContainer } from './components/App/Container';
+import './index.css';
+import { unregister } from './registerServiceWorker';
 
-// const bugsnagClient = bugsnag("87e28aed7927156bee7f8accd10ed20a");
-// bugsnagClient.use(bugsnagReact, React);
-// const ErrorBoundary = bugsnagClient.getPlugin("react");
+const bugsnagClient = bugsnag('87e28aed7927156bee7f8accd10ed20a');
+bugsnagClient.use(bugsnagReact, React);
+const ErrorBoundary = bugsnagClient.getPlugin('react');
 
-ReactDOM.render(
-  // <ErrorBoundary>
+const App =
+  process.env.NODE_ENV === 'development' ? (
     <AppContainer />
-  // </ErrorBoundary>,
- , document.getElementById("root") as HTMLElement
-);
+  ) : (
+    <ErrorBoundary>
+      <AppContainer />
+    </ErrorBoundary>
+  );
+
+ReactDOM.render(App, document.getElementById('root') as HTMLElement);
 unregister();
