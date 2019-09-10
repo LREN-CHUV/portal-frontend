835dbd5354b8cedb16ff1dcbe4a9b30274c85d51
Manuel Spuhler <manuel.spuhler@chuv.ch>
2019-09-10 09:12:46 +0200

    Splash screen wip

--- src/components/UI/Splash.tsx
+++ src/components/UI/Splash.tsx
@@ -22,11 +22,11 @@ const MainBox = styled.div`
 
   img {
     margin: 0 auto;
+    background-color: #ffffff00;
   }
 
   section {
     margin: 0;
-    padding: 0 8em;
   }
 
   h1,
@@ -49,6 +49,24 @@ const Login = styled.button`
 
 const Columns = styled.div`
   display: flex;
+  padding: 0 16em;
+
+  section {
+    flex: 1;
+    padding: 0 1em;
+  }
+
+  div {
+    flex: 1;
+
+    h3 {
+      margin: 0;
+    }
+
+    ul {
+      padding: 0.5em 0;
+    }
+  }
 `;
 
 export default ({
@@ -70,63 +88,65 @@ export default ({
     <MainBox>
       <Login onClick={handleLoginPress}>Login</Login>
       <img alt="HBP logo" title={'Human Brain Project'} src={HBPLogo} />
+      <h1>THE MEDICAL INFORMATICS PLATFORM</h1>
 
-      <section>
-        <h1>THE MEDICAL INFORMATICS PLATFORM</h1>
-        <p>
-          Thousands of brain images and terabytes of invaluable associated
-          medical data are produced every day at a gigantic pace around the
-          world. The Medical Informatics Platform (MIP) aims to federate this
-          information and provide the tools to the experts to effectively
-          analyse it and advance more rapidly in understanding the neurological
-          and psychiatric diseases. This will in turn allow identifying the
-          biological changes associated and open real possibilities for early
-          diagnosis and personalised medicine.
-        </p>
-        <p>
-          The MIP provides methods to analyse federated data from hospitals,
-          research centres and biobanks. Clinical scientists can develop, share
-          and release results of their research. The MIP aims to bring together
-          people across professional and scientific fields encourages them to
-          actively contribute to the design and development of the services
-          which the MIP provides.
-        </p>
-      </section>
       <Columns>
         <section>
-          <h3>The MIP has three main goals:</h3>
-          <ul>
-            <li>
-              Build the tools to federate clinical data, currently inaccessible
-              outside hospital and research archives;
-            </li>
-            <li>
-              Recruit hospitals to contribute to and benefit by using the
-              platform;
-            </li>
-            <li>
-              Develop tools for extracting biological signatures of diseases
-              from multi-level data.
-            </li>
-          </ul>
+          <p>
+            Thousands of brain images and terabytes of invaluable associated
+            medical data are produced every day at a gigantic pace around the
+            world. The Medical Informatics Platform (MIP) aims to federate this
+            information and provide the tools to the experts to effectively
+            analyse it and advance more rapidly in understanding the
+            neurological and psychiatric diseases. This will in turn allow
+            identifying the biological changes associated and open real
+            possibilities for early diagnosis and personalised medicine.
+          </p>
+          <p>
+            The MIP provides methods to analyse federated data from hospitals,
+            research centres and biobanks. Clinical scientists can develop,
+            share and release results of their research. The MIP aims to bring
+            together people across professional and scientific fields encourages
+            them to actively contribute to the design and development of the
+            services which the MIP provides.
+          </p>
         </section>
-        <section>
-          <h3>You can use the MIP</h3>
-          <ul>
-            <li>
-              as a clinician, for objective diagnoses and treatment of brain
-              disease;
-            </li>
-            <li>
-              as a neuroscientist, for the application and testing of new models
-              and methods;
-            </li>
-            <li>
-              as a pharmaceutical or biotech company, for disease target
-              discovery.
-            </li>
-          </ul>
-        </section>
+        <div>
+          <section>
+            <h3>The MIP has three main goals:</h3>
+            <ul>
+              <li>
+                Build the tools to federate clinical data, currently
+                inaccessible outside hospital and research archives;
+              </li>
+              <li>
+                Recruit hospitals to contribute to and benefit by using the
+                platform;
+              </li>
+              <li>
+                Develop tools for extracting biological signatures of diseases
+                from multi-level data.
+              </li>
+            </ul>
+          </section>
+          <section>
+            <h3>You can use the MIP</h3>
+            <ul>
+              <li>
+                as a clinician, for objective diagnoses and treatment of brain
+                disease;
+              </li>
+              <li>
+                as a neuroscientist, for the application and testing of new
+                models and methods;
+              </li>
+              <li>
+                as a pharmaceutical or biotech company, for disease target
+                discovery.
+              </li>
+            </ul>
+          </section>
+        </div>
       </Columns>
     </MainBox>
   );
