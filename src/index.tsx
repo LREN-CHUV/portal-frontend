bb860b7df00252f357ae52971eff8031920631e5
Manuel Spuhler <manuel.spuhler@chuv.ch>
2019-09-27 16:33:16 +0200

    Bugsnag isolation

--- src/index.tsx
+++ src/index.tsx
@@ -6,18 +6,20 @@ import { default as AppContainer } from './components/App/Container';
 import './index.css';
 import { unregister } from './registerServiceWorker';
 
-const bugsnagClient = bugsnag('87e28aed7927156bee7f8accd10ed20a');
-bugsnagClient.use(bugsnagReact, React);
-const ErrorBoundary = bugsnagClient.getPlugin('react');
+const AppBox = (): JSX.Element => {
+  if (process.env.NODE_ENV === 'production') {
+    const bugsnagClient = bugsnag('87e28aed7927156bee7f8accd10ed20a');
+    bugsnagClient.use(bugsnagReact, React);
+    const ErrorBoundary = bugsnagClient.getPlugin('react');
+    return (
+      <ErrorBoundary>
+        <AppContainer />
+      </ErrorBoundary>
+    );
+  }
 
-const App =
-  process.env.NODE_ENV !== 'production' ? (
-    <AppContainer />
-  ) : (
-    <ErrorBoundary>
-      <AppContainer />
-    </ErrorBoundary>
-  );
+  return <AppContainer />;
+};
 
-ReactDOM.render(App, document.getElementById('root') as HTMLElement);
+ReactDOM.render(AppBox(), document.getElementById('root') as HTMLElement);
 unregister();
