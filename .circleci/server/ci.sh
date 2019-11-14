855173ebb881ea84a54d6aa0919cfc5d288b2e2e
Manuel Spuhler <manuel.spuhler@chuv.ch>
2019-11-14 16:57:37 +0100

    circleci: build exareme swarm #1

--- .circleci/server/ci.sh
+++ .circleci/server/ci.sh
@@ -7,113 +7,135 @@
 #   --all: execute the full suite of tests, including slow tests such as Chaos testing
 #
 
-set -o pipefail  # trace ERR through pipes
-set -o errtrace  # trace ERR through 'time command' and other functions
-set -o errexit   ## set -e : exit the script if any statement returns a non-true return value
+set -o pipefail # trace ERR through pipes
+set -o errtrace # trace ERR through 'time command' and other functions
+set -o errexit  ## set -e : exit the script if any statement returns a non-true return value
+
+if groups $USER | grep &>/dev/null '\bdocker\b'; then
+  DOCKER="docker"
+else
+  DOCKER="$DOCKER"
+fi
 
 # This script is used for publish and continuous integration.
 
-get_script_dir () {
-     SOURCE="${BASH_SOURCE[0]}"
+#!/usr/bin/env bash
+# Key-Value Store
+EXAREME_KEYSTORE="exareme-keystore:8500"
 
-     while [ -h "$SOURCE" ]; do
-          DIR="$( cd -P "$( dirname "$SOURCE" )" && pwd )"
-          SOURCE="$( readlink "$SOURCE" )"
-          [[ $SOURCE != /* ]] && SOURCE="$DIR/$SOURCE"
-     done
-     cd -P "$( dirname "$SOURCE" )"
-     pwd
-}
+# Docker internal folder for the Exareme data
+DOCKER_DATA_FOLDER="/root/exareme/data/"
 
-cd "$(get_script_dir)"
+# Portainer
+PORTAINER_PORT="9000"
+PORTAINER_IMAGE="portainer/portainer"
+PORTAINER_VERSION=":latest"
+PORTAINER_DATA=$(echo $PWD)"/portainer"
 
-for param in "$@"
-do
-  if [ "--all" == "$param" ]; then
-    test_args=""
-    echo "INFO: ---all option detected !"
-  fi
-done
+FEDERATION_ROLE="master"
 
-if pgrep -lf sshuttle > /dev/null ; then
-  echo "sshuttle detected. Please close this program as it messes with networking and prevents Docker links to work"
-  exit 1
-fi
-
-if [[ $NO_SUDO || -n "$CIRCLECI" ]]; then
-  DOCKER_COMPOSE="docker-compose --project-name webanalyticsstarter -f docker-compose-ci.yml"
-elif groups "$USER" | grep &>/dev/null '\bdocker\b'; then
-  DOCKER_COMPOSE="docker-compose --project-name webanalyticsstarter -f docker-compose-ci.yml"
+#Check if data_path exist
+if [[ -s dataPath.txt ]]; then
+  :
 else
-  DOCKER_COMPOSE="sudo docker-compose --project-name webanalyticsstarter -f docker-compose-ci.yml"
+  echo "What is the data_path for host machine?"
+  read answer
+  #Check that path ends with /
+  if [[ "${answer: -1}" != "/" ]]; then
+    answer=${answer}"/"
+  fi
+  echo LOCAL_DATA_FOLDER=${answer} >dataPath.txt
 fi
 
-function _cleanup() {
-  local error_code="$?"
-  echo "Stopping the containers..."
-  $DOCKER_COMPOSE stop | true
-  $DOCKER_COMPOSE down | true
-  $DOCKER_COMPOSE rm -f > /dev/null 2> /dev/null | true
-  exit $error_code
-}
-#trap _cleanup EXIT INT TERM
+LOCAL_DATA_FOLDER=$(cat dataPath.txt | cut -d '=' -f 2)
 
-echo "Remove old running containers (if any)..."
-$DOCKER_COMPOSE kill
-$DOCKER_COMPOSE rm -f
+chmod 755 *.sh
 
-echo "Deploy a Postgres server and wait for it to be ready..."
-$DOCKER_COMPOSE up -d db zookeeper
-$DOCKER_COMPOSE run wait_zookeeper
-$DOCKER_COMPOSE up -d mesos_master
-$DOCKER_COMPOSE run wait_mesos_master
-$DOCKER_COMPOSE up -d mesos_slave
-$DOCKER_COMPOSE run wait_dbs
+#Check if Exareme docker image exists in file
+if [[ -s exareme.yaml ]]; then
+  :
+else
+  . ./exareme.sh
+fi
 
-echo "Create databases..."
-$DOCKER_COMPOSE run create_dbs
+if [[ $($DOCKER info | grep Swarm | grep inactive*) != '' ]]; then
+  echo -e "\nInitialize Swarm.."
+  $DOCKER swarm init --advertise-addr=$(wget http://ipinfo.io/ip -qO -)
+else
+  echo -e "\nLeaving previous Swarm.."
+  $DOCKER swarm leave -f
+  sleep 1
+  echo -e "\nInitialize Swarm.."
+  $DOCKER swarm init --advertise-addr=$(wget http://ipinfo.io/ip -qO -)
+fi
 
-echo "Migrate woken database..."
-$DOCKER_COMPOSE run woken_db_setup
+if [[ $($DOCKER network ls | grep mip-local) == '' ]]; then
+  echo -e "\nInitialize Network"
+  $DOCKER network create \
+    --driver=overlay --opt encrypted --subnet=10.20.30.0/24 --ip-range=10.20.30.0/24 --gateway=10.20.30.254 mip-local
+fi
 
-echo "Migrate metadata database..."
-$DOCKER_COMPOSE run sample_meta_db_setup
+#Get hostname of node
+name=$(hostname)
 
-echo "Migrate features database..."
-$DOCKER_COMPOSE run sample_data_db_setup
+echo -e "\nUpdate label name for Swarm node "$name
+$DOCKER node update --label-add name=${name} ${name}
+echo -e "\n"
 
-echo "Run containers..."
-for i in 1 2 3 4 5 ; do
-  $DOCKER_COMPOSE up -d chronos
-  $DOCKER_COMPOSE run wait_chronos
-  $DOCKER_COMPOSE logs chronos | grep java.util.concurrent.TimeoutException || break
-  echo "Chronos failed to start, restarting..."
-  $DOCKER_COMPOSE stop chronos
-done
+#Read image from file exareme.yaml
+# image=""
+# while read -r line; do
+#   if [[ ${line:0:1} == "#" ]] || [[ -z ${line} ]]; then #comment line or empty line, continue
+#     continue
+#   fi
 
-$DOCKER_COMPOSE up -d woken
-$DOCKER_COMPOSE run wait_woken
+#   image=$(echo ${image})$(echo "$line" | cut -d ':' -d ' ' -d '"' -f 2 -d '"')":"
 
-$DOCKER_COMPOSE up -d wokenvalidation
-$DOCKER_COMPOSE run wait_wokenvalidation
+# done <exareme.yaml
 
-for i in 1 2 3 4 5 ; do
-  $DOCKER_COMPOSE logs chronos | grep java.util.concurrent.TimeoutException || break
-  echo "Chronos failed to start, restarting..."
-  $DOCKER_COMPOSE stop chronos
-  $DOCKER_COMPOSE up -d chronos
-  $DOCKER_COMPOSE run wait_chronos
-done
+# #remove the last : from string
+# image=${image:0:-1}
 
-echo "The Algorithm Factory is now running on your system"
+# #imageName the first half of string image
+# imageName=$(echo "$image" | cut -d ':' -f 1)
 
-FRONTEND_URL=http://frontend \
-  $DOCKER_COMPOSE up -d portalbackend
-$DOCKER_COMPOSE run wait_portal_backend
-$DOCKER_COMPOSE up -d frontend
+# #tag the second half of string image
+# tag=$(echo "$image" | cut -d ':' -f 2)
+# echo $image
+# echo $imageName
+# echo $tag
 
-echo "[OK] System startup success!"
+imageName="hbpmip/exareme"
+tag="v21.2.0"
 
-echo
-# Cleanup
-#_cleanup
+#Remove services if already existed
+if [[ $($DOCKER service ls | grep ${name}"_exareme-keystore") != '' ]]; then
+  $DOCKER service rm ${name}"_exareme-keystore"
+fi
+
+if [[ $($DOCKER service ls | grep ${name}"_exareme-master") != '' ]]; then
+  $DOCKER service rm ${name}"_exareme-master"
+fi
+
+env FEDERATION_NODE=${name} FEDERATION_ROLE=${FEDERATION_ROLE} EXAREME_IMAGE=${imageName}":"${tag} \
+  EXAREME_KEYSTORE=${EXAREME_KEYSTORE} DOCKER_DATA_FOLDER=${DOCKER_DATA_FOLDER} \
+  LOCAL_DATA_FOLDER=${LOCAL_DATA_FOLDER} \
+  $DOCKER stack deploy -c docker-compose-master.yml ${name}
+
+# echo -e "\nDo you wish to run Portainer service? [ y/n ]"
+# read answer
+
+# while true; do
+#   if [[ ${answer} == "y" ]]; then
+#     if [[ $($DOCKER service ls | grep mip_portainer) != '' ]]; then
+#       $DOCKER service rm mip_portainer
+#     fi
+#     . ./portainer.sh
+#     break
+#   elif [[ ${answer} == "n" ]]; then
+#     break
+#   else
+#     echo ${answer}" is not a valid answer. Try again [ y/n ]"
+#     read answer
+#   fi
+# done
