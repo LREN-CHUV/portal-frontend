5af545b2ebe52004d7b7df8dbbf45183c2a072f5
Manuel Spuhler <manuel.spuhler@chuv.ch>
2019-12-02 17:37:36 +0100

    Kmeans test

---
+++ src/components/API/__tests__/Integration/ExaremeKMeans.test.tsx
@@ -0,0 +1,108 @@
+import { mount } from 'enzyme';
+import * as React from 'react';
+
+import Result from '../../../Result/Result';
+import { VariableEntity } from '../../Core';
+import { ExperimentPayload } from '../../Experiment';
+import {
+  createExaremePayload,
+  createExperiment,
+  createModel,
+  getDatasets,
+  waitForResult
+} from '../../Utils';
+
+// config
+
+const modelSlug = `anova-${Math.round(Math.random() * 10000)}`;
+const experimentCode = 'KMEANS';
+const parameters = [
+  { code: 'k', value: '4' },
+  { code: 'e', value: 1 },
+  { code: 'iterations_max_number', value: 1000 },
+  { code: 'pathology', value: 'dementia' }
+];
+
+const model: any = (datasets: VariableEntity[]) => ({
+  query: {
+    pathology: 'dementia', // FIXME: should by dynamic
+    coVariables: [],
+    filters: '',
+    testingDatasets: [],
+    trainingDatasets: datasets.map(d => ({
+      code: d.code
+    })),
+    validationDatasets: [],
+    variables: [
+      {
+        code: 'leftacgganteriorcingulategyrus'
+      },
+      {
+        code: 'rightcerebellumexterior'
+      }
+    ]
+  }
+});
+
+// Test
+
+describe('Integration Test for experiment API', () => {
+  let datasets: VariableEntity[] | undefined;
+
+  beforeAll(async () => {
+    datasets = await getDatasets();
+    expect(datasets).toBeTruthy();
+
+    const mstate = await createModel({
+      model: model(datasets),
+      modelSlug
+    });
+
+    expect(mstate.error).toBeFalsy();
+    expect(mstate.model).toBeTruthy();
+
+    return datasets !== undefined && mstate.model !== undefined;
+  });
+
+  it(`create ${experimentCode}`, async () => {
+    if (!datasets) {
+      throw new Error('datasets not defined');
+    }
+    const payload: ExperimentPayload = createExaremePayload(
+      model,
+      datasets,
+      experimentCode,
+      parameters,
+      modelSlug
+    );
+
+    const { error, experiment } = await createExperiment({
+      experiment: payload
+    });
+
+    expect(error).toBeFalsy();
+    expect(experiment).toBeTruthy();
+
+    const uuid = experiment && experiment.uuid;
+    expect(uuid).toBeTruthy();
+    if (!uuid) {
+      throw new Error('uuid not defined');
+    }
+
+    const experimentState = await waitForResult({ uuid });
+    expect(experimentState.error).toBeFalsy();
+    expect(experimentState.experiment).toBeTruthy();
+
+    const props = { experimentState };
+    const wrapper = mount(<Result {...props} />);
+    expect(wrapper.find('.error')).toHaveLength(0);
+    expect(wrapper.find('.loading')).toHaveLength(0);
+    expect(wrapper.find('.result')).toHaveLength(3);
+    expect(
+      wrapper
+        .find('div.result table tbody tr td')
+        .at(1)
+        .text()
+    ).toEqual('4.123');
+  });
+});
