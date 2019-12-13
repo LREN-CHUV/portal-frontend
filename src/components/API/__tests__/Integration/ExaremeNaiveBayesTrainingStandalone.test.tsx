31ce6cb940a27ac5a9c80e3efd4647d4d0993972
Manuel Spuhler <manuel.spuhler@chuv.ch>
2019-12-13 09:29:45 +0100

    Naive Bayes integrated. !released. Output to be fixed

---
+++ src/components/API/__tests__/Integration/ExaremeNaiveBayesTrainingStandalone.test.tsx
@@ -0,0 +1,122 @@
+import { mount } from 'enzyme';
+import * as React from 'react';
+
+import Result from '../../../Result/Result';
+import { VariableEntity } from '../../Core';
+import { ExperimentPayload } from '../../Experiment';
+import {
+  createExaremePayload,
+  createExperiment,
+  createModel,
+  getDatasets,
+  waitForResult
+} from '../../Utils';
+
+// config
+
+const modelSlug = `linear-${Math.round(Math.random() * 10000)}`;
+const experimentCode = 'NAIVE_BAYES_TRAINING_STANDALONE';
+const parameters = [
+  {
+    code: 'alpha',
+    value: '0'
+  },
+  { code: 'pathology', value: 'dementia' }
+];
+
+const model: any = (datasets: VariableEntity[]) => ({
+  query: {
+    // FIXME: should by dynamic
+    coVariables: [
+      {
+        code: 'leftacgganteriorcingulategyrus'
+      }
+    ],
+    filters: '',
+    groupings: [],
+    pathology: 'dementia',
+    testingDatasets: [],
+    trainingDatasets: datasets.map(d => ({
+      code: d.code
+    })),
+    validationDatasets: [],
+    variables: [
+      {
+        code: 'alzheimerbroadcategory'
+      }
+    ]
+  }
+});
+
+// Test
+
+describe('Integration Test for experiment API', () => {
+  let datasets: VariableEntity[] | undefined;
+
+  beforeAll(async () => {
+    datasets = await getDatasets();
+    datasets = datasets && datasets.filter((_, i) => i === 0)
+    expect(datasets).toBeTruthy();
+
+    const mstate = await createModel({
+      model: model(datasets),
+      modelSlug
+    });
+
+    expect(mstate.error).toBeFalsy();
+    expect(mstate.model).toBeTruthy();
+
+    return datasets !== undefined && mstate.model !== undefined;
+  });
+
+  it(`create ${experimentCode}`, async () => {
+    if (!datasets) {
+      throw new Error('datasets not defined');
+    }
+    const payload: ExperimentPayload = createExaremePayload(
+      model,
+      datasets,
+      experimentCode,
+      parameters,
+      modelSlug
+    );
+    const { error, experiment } = await createExperiment({
+      experiment: payload
+    });
+
+    expect(error).toBeFalsy();
+    expect(experiment).toBeTruthy();
+
+    const uuid = experiment && experiment.uuid;
+    expect(uuid).toBeTruthy();
+    if (!uuid) {
+      throw new Error('uuid not defined');
+    }
+
+    const experimentState = await waitForResult({ uuid });
+    expect(experimentState.error).toBeFalsy();
+    expect(experimentState.experiment).toBeTruthy();
+
+    const props = { experimentState };
+    const wrapper = mount(<Result {...props} />);
+    expect(wrapper.find('.error')).toHaveLength(0);
+    expect(wrapper.find('.loading')).toHaveLength(0);
+    expect(wrapper.find('.result')).toHaveLength(2);
+    // expect(
+    //   wrapper
+    //     .find('div.result table tbody tr td')
+    //     .at(1)
+    //     .first()
+    //     .text()
+    // ).toEqual('0.937');
+    // expect(
+    //   wrapper
+    //     .find('div.result')
+    //     .at(1)
+    //     .find('table tbody tr td')
+    //     .at(1)
+    //     .first()
+    //     .text()
+    // ).toEqual('-1.455');
+  });
+});
