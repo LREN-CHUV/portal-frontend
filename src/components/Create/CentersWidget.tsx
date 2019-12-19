8adc2f646d784c23e2b6bb4579e5fdf27723eccc
Manuel Spuhler <manuel.spuhler@chuv.ch>
2019-12-19 14:02:10 +0100

    KMeans without centers

---
+++ src/components/Create/CentersWidget.tsx
@@ -0,0 +1,41 @@
+import React, { useCallback, useEffect, useState } from 'react';
+import { APICore } from '../API';
+import { Query } from '../API/Model';
+import { Col, Form, FormControl, FormGroup, Row } from 'react-bootstrap';
+
+interface Props {
+  apiCore: APICore;
+  parameterName: string;
+  query?: Query;
+  handleChangeCentersParameter: any;
+}
+export default ({
+  apiCore,
+  query,
+  parameterName,
+  handleChangeCentersParameter
+}: Props): JSX.Element => {
+  const lookupCallback = useCallback(apiCore.lookup, []);
+
+  const variables = (query && query.variables) || [];
+
+  return (
+    <>
+      {variables.map((c1, i) => (
+        <div key={c1.code}>
+          <Col sm={12}>
+            {variables.map((c2, i) => (
+              <FormControl
+                key={c2.code}
+                type="number"
+                // tslint:disable-next-line jsx-no-lambda
+                placeholder={c2.code}
+                onChange={event => handleChangeCentersParameter()}
+              />
+            ))}
+          </Col>
+        </div>
+      ))}
+    </>
+  );
+};
