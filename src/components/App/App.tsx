6d44807091a800b07a3aab4164d65bf48dd720b3
Manuel Spuhler <manuel.spuhler@gmail.com>
2020-02-18 21:07:42 +0100

    Histograms x<->y

--- src/components/App/App.tsx
+++ src/components/App/App.tsx
@@ -1,16 +1,13 @@
 import 'bootstrap/dist/css/bootstrap.css';
-
 import * as React from 'react';
 import { Route, Switch } from 'react-router-dom';
 import styled, { createGlobalStyle } from 'styled-components';
-
 import backgroundImage from '../../images/body-bg.jpg';
 import ExperimentReview from '../Analysis/Container';
 import { APICore, APIExperiment, APIMining, APIModel, APIUser } from '../API';
 import { ExperimentResponse } from '../API/Experiment';
 import Article from '../Article/Container';
 import ExperimentCreate from '../Create/Container';
-import Home from '../Dashboard/Container';
 import Explore from '../Explore/Container';
 import Help from '../Help/Help';
 import ExperimentResult from '../Result/Container';
