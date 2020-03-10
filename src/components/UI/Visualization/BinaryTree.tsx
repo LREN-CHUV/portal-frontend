2a5534b75bb9fe3e5f000df133a3bd4cd227575c
Manuel Spuhler <manuel.spuhler@gmail.com>
2020-03-10 10:44:17 +0100

    Update rendering Cart output

--- src/components/UI/Visualization/BinaryTree.tsx
+++ src/components/UI/Visualization/BinaryTree.tsx
@@ -5,7 +5,7 @@ import styled from 'styled-components';
 import { round } from '../../utils';
 
 const nodeRectSize = { width: 160, height: 80 };
-const svgSize = { width: 800, height: 1000, margin: 100, vmargin: 10 };
+const fixedSize = { w: 800, h: 600 };
 
 const SVGContainer = styled.div`
   width: 100%;
@@ -93,109 +93,132 @@ const makeNodeData = (data: TreeNode): NodeData => ({
 export default ({ data }: { data: TreeNode }): JSX.Element => {
   const svgRef = useRef(null);
   const isFirstRender = useRef(false);
+  const [, setLocalData] = React.useState(data);
 
   React.useLayoutEffect(() => {
-    if (!svgRef.current || !data || isFirstRender.current) {
+    if (!svgRef.current || !data) {
       return;
     }
 
-    isFirstRender.current = true;
+    const firstRender = (): void => {
+      const nextData = {
+        childnodes: makeNodes(data),
+        info: makeNodeData(data)
+      };
 
-    const nextData = {
-      childnodes: makeNodes(data),
-      info: makeNodeData(data)
+      const treemap = d3
+        .tree()
+        .separation(() => 1.2)
+        .nodeSize([nodeRectSize.width, nodeRectSize.height * 2]);
+
+      const root = d3.hierarchy(nextData, (d: any) => d.childnodes);
+      const nodes = treemap(root);
+
+      const svg = d3
+        .select(svgRef.current)
+        .append<Element>('svg')
+        .attr('width', '100%')
+        .attr('height', '100%')
+        .attr('preserveAspectRatio', 'none')
+        .attr('viewBox', `0 0 ${fixedSize.w} ${fixedSize.h}`)
+        .call(
+          d3
+            .zoom()
+            .scaleExtent([0, 10])
+            .on('zoom', () => g.attr('transform', d3.event.transform))
+        )
+        .append('svg:g')
+        .attr(
+          'transform',
+          `translate(${fixedSize.w / 2},${nodeRectSize.height}) scale(0.5)`
+        );
+
+      const g = svg.append('g');
+
+      // adds the links between the nodes
+      g.selectAll('.link')
+        .data(nodes.descendants().slice(1))
+        .enter()
+        .append('path')
+        .attr('class', 'link')
+        .attr(
+          'd',
+          (d: any) =>
+            `M ${d.x}, ${d.y} C ${d.x}, ${(d.y + d.parent.y) / 2} ${
+              d.parent.x
+            }, ${(d.y + d.parent.y) / 2} ${d.parent.x}, ${d.parent.y}`
+        );
+
+      // adds each node as a group
+      const node = g
+        .selectAll('.node')
+        .data(nodes.descendants())
+        .enter()
+        .append('g')
+        .attr('class', 'node')
+        .attr('transform', (d: any) => `translate(${d.x},${d.y})`);
+
+      node
+        .append('rect')
+        .attr('x', -nodeRectSize.width / 2)
+        .attr('y', -nodeRectSize.height / 2)
+        .attr('width', nodeRectSize.width)
+        .attr('height', nodeRectSize.height);
+
+      const x = -nodeRectSize.width / 2 + 10;
+      const y = -nodeRectSize.height / 2 + 20;
+
+      node
+        .append('text')
+        .attr('x', x)
+        .attr('y', y)
+        .style('text-anchor', (d: any) => 'start')
+        .text((d: any) => d.data.info?.variable)
+        .append('tspan')
+        .attr('x', x)
+        .attr('y', y + 12)
+        .text((d: any) => d.data.info?.criterion)
+        .append('tspan')
+        .attr('x', x)
+        .attr('y', y + 24)
+        .text((d: any) => d.data.info?.samples)
+        .append('tspan')
+        .attr('x', x)
+        .attr('y', y + 36)
+        .text((d: any) => d.data.info?.value)
+        .append('tspan')
+        .attr('x', x)
+        .attr('y', y + 50)
+        .text((d: any) => d.data.info?.class);
     };
 
-    console.log(nextData);
+    const updateRender = (): void => {
+      d3.select(svgRef.current)
+        .selectAll('svg')
+        .remove();
 
-    const width = svgSize.width;
-    const height = svgSize.height;
+      firstRender();
+    };
 
-    const treemap = d3
-      .tree()
-      .separation(() => 1.2)
-      .nodeSize([nodeRectSize.width, nodeRectSize.height * 2]);
+    // update or render
+    setLocalData(previousData => {
+      if (previousData !== data) {
+        updateRender();
 
-    const root = d3.hierarchy(nextData, (d: any) => d.childnodes);
-    const nodes = treemap(root);
+        return previousData;
+      } else {
+        firstRender();
+        return data;
+      }
+    });
 
-    const svg = d3
-      .select(svgRef.current)
-      .append<Element>('svg')
-      .attr('preserveAspectRatio', 'xMinYMin meet')
-      .attr('viewBox', `0 0 ${width} ${height}`)
-      .call(
-        d3
-          .zoom()
-          .scaleExtent([0, 10])
-          .on('zoom', () => g.attr('transform', d3.event.transform))
-      );
-
-    const g = svg
-      .append('g')
-      .attr('transform', `translate(${width / 2},${nodeRectSize.height})`);
-
-    // adds the links between the nodes
-    g.selectAll('.link')
-      .data(nodes.descendants().slice(1))
-      .enter()
-      .append('path')
-      .attr('class', 'link')
-      .attr(
-        'd',
-        (d: any) =>
-          `M ${d.x}, ${d.y} C ${d.x}, ${(d.y + d.parent.y) / 2} ${
-            d.parent.x
-          }, ${(d.y + d.parent.y) / 2} ${d.parent.x}, ${d.parent.y}`
-      );
-
-    // adds each node as a group
-    const node = g
-      .selectAll('.node')
-      .data(nodes.descendants())
-      .enter()
-      .append('g')
-      .attr('class', 'node')
-      .attr('transform', (d: any) => `translate(${d.x},${d.y})`);
-
-    node
-      .append('rect')
-      .attr('x', -nodeRectSize.width / 2)
-      .attr('y', -nodeRectSize.height / 2)
-      .attr('width', nodeRectSize.width)
-      .attr('height', nodeRectSize.height);
-
-    const x = -nodeRectSize.width / 2 + 10;
-    const y = -nodeRectSize.height / 2 + 20;
-
-    node
-      .append('text')
-      .attr('x', x)
-      .attr('y', y)
-      .style('text-anchor', (d: any) => 'start')
-      .text((d: any) => d.data.info?.variable)
-      .append('tspan')
-      .attr('x', x)
-      .attr('y', y + 12)
-      .text((d: any) => d.data.info?.criterion)
-      .append('tspan')
-      .attr('x', x)
-      .attr('y', y + 24)
-      .text((d: any) => d.data.info?.samples)
-      .append('tspan')
-      .attr('x', x)
-      .attr('y', y + 36)
-      .text((d: any) => d.data.info?.value)
-      .append('tspan')
-      .attr('x', x)
-      .attr('y', y + 50)
-      .text((d: any) => d.data.info?.class);
+    isFirstRender.current = true;
+    updateRender();
   }, [data]);
 
   return (
     <div>
       <SVGContainer ref={svgRef} />
-      <pre>{data && JSON.stringify(data, null, 4)}</pre>
     </div>
   );
 };
