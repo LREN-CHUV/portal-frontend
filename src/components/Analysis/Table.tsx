b7665e42f8f11babf16576ffc376e781fa4c1bb7
Manuel Spuhler <manuel.spuhler@gmail.com>
2020-06-05 15:18:26 +0200

    Analytics demo

--- src/components/Analysis/Table.tsx
+++ src/components/Analysis/Table.tsx
@@ -6,7 +6,7 @@ import './Table.css';
 import { Column } from 'primereact/column';
 import { DataTable } from 'primereact/datatable';
 import * as React from 'react';
-
+import { Tab, Tabs } from 'react-bootstrap';
 import { Variable, VariableEntity } from '../API/Core';
 import { MiningResponse } from '../API/Mining';
 import { Query } from '../API/Model';
@@ -35,7 +35,7 @@ const variableTemplate = (rowData: any, column: any) => (
   </span>
 );
 
-const computeResults = ({
+const computeSingleResults = ({
   summaryStatistics,
   selectedDatasets,
   variables
@@ -85,7 +85,7 @@ const computeResults = ({
     const mining =
       summaryStatistics &&
       summaryStatistics.length > 0 &&
-      summaryStatistics[0].data.single;
+      summaryStatistics[0].data['single'];
 
     // fetch results by datasets
     selectedDatasets.forEach(dataset => {
@@ -144,6 +144,113 @@ const computeResults = ({
   return rows;
 };
 
+const computeModelResults = ({
+  summaryStatistics,
+  selectedDatasets,
+  variables
+}: {
+  summaryStatistics?: MiningResponse[];
+  selectedDatasets?: VariableEntity[];
+  variables?: VariableEntity[];
+}): TableRow[] => {
+  const computedRows: TableRow[] = [];
+
+  if (!variables || variables.length === 0) {
+    computedRows.push({ variable: 'Select some variables' });
+    return computedRows;
+  }
+
+  if (!selectedDatasets) {
+    computedRows.push({ variable: 'Select a dataset' });
+    return computedRows;
+  }
+
+  const rows: TableRow[] = [];
+  variables.forEach(variable => {
+    const isMultinominal =
+      variable.type === 'multinominal' || variable.type === 'binominal';
+    const row: TableRow = {};
+    let polynominalRows: TableRow[] = [];
+
+    // set labels, Variables + multinominal categories rows
+    if (isMultinominal && variable.enumerations) {
+      row.variable = variable.label;
+      polynominalRows = variable.enumerations.reduce(
+        (acc: any[], val: any) => [
+          ...acc,
+          {
+            category: {
+              code: val.code,
+              label: val.label || val.code
+            }
+          }
+        ],
+        []
+      );
+    } else {
+      row.variable = variable.label;
+    }
+
+    const mining =
+      summaryStatistics &&
+      summaryStatistics.length > 0 &&
+      summaryStatistics[0].data['model'];
+
+    // fetch results by datasets
+    selectedDatasets.forEach(dataset => {
+      const datasetCode = dataset.code;
+      if (!mining) {
+        row[datasetCode] = 'loading...';
+        if (isMultinominal) {
+          polynominalRows = polynominalRows.map(r => ({
+            ...r,
+            [datasetCode]: 'loading...'
+          }));
+        }
+      } else {
+        const rowVariable = mining[dataset.code];
+        const rowData = rowVariable?.data[variable.code];
+
+        if (!rowData) {
+          row[datasetCode] = 'loading...';
+        } else {
+          if (isMultinominal) {
+            const data = rowData;
+            if (data === PRIVACY_ERROR) {
+              row[datasetCode] = data;
+              polynominalRows = polynominalRows.map(r => ({
+                ...r,
+                [datasetCode]: PRIVACY_ERROR
+              }));
+            } else {
+              row[datasetCode] = rowData.num_datapoints;
+              polynominalRows = polynominalRows.map(r => ({
+                ...r,
+                [datasetCode]: r?.category ? rowData[r?.category?.code] : ''
+              }));
+            }
+          } else {
+            const data = rowData;
+            if (data === PRIVACY_ERROR) {
+              row[datasetCode] = data;
+            } else {
+              const getRound = (value: number): string => round(value, 2);
+              row[datasetCode] = `${getRound(data.mean)} (${getRound(
+                data.min
+              )} - ${getRound(data.max)}) - std: ${getRound(data.std)}`;
+            }
+          }
+        }
+      }
+    });
+
+    rows.push(row);
+    polynominalRows.forEach(r => rows.push(r));
+  });
+
+  return rows;
+};
+
 const Table = ({
   summaryStatistics,
   selectedDatasets,
@@ -158,7 +265,13 @@ const Table = ({
       ...(query.groupings || [])
     ].map(v => lookup(v.code));
 
-  const rows = computeResults({
+  const rows = computeSingleResults({
+    summaryStatistics,
+    selectedDatasets,
+    variables
+  });
+
+  const rows2 = computeModelResults({
     summaryStatistics,
     selectedDatasets,
     variables
@@ -182,6 +295,32 @@ const Table = ({
       ]
     : [];
 
+  const columns2 = selectedDatasets
+    ? [
+        <Column
+          header="VARIABLES"
+          field="variable"
+          key={'variable'}
+          body={variableTemplate}
+        />,
+        ...selectedDatasets.map((dataset: Variable) => {
+          const mining =
+            summaryStatistics &&
+            summaryStatistics.length > 0 &&
+            summaryStatistics[0].data['model'];
+          const sum = mining && mining[dataset.code];
+          const total = (sum && sum.num_datapoints) || '';
+          return (
+            <Column
+              header={`${dataset.label || dataset.code} (${total})`}
+              field={dataset.code}
+              key={dataset.code}
+            />
+          );
+        })
+      ]
+    : [];
+
   const error =
     summaryStatistics &&
     summaryStatistics.find((r: any) => ERRORS_OUTPUT.includes(r.type));
@@ -189,7 +328,16 @@ const Table = ({
   return (
     <>
       {error && <Error message={error.data} />}
-      {!error && <DataTable value={rows}>{columns}</DataTable>}
+      {!error && (
+        <Tabs defaultActiveKey={1} id="uncontrolled-mining-tab">
+          <Tab eventKey={1} title="Single">
+            <DataTable value={rows}>{columns}</DataTable>
+          </Tab>
+          <Tab eventKey={2} title="Model">
+            <DataTable value={rows2}>{columns2}</DataTable>
+          </Tab>
+        </Tabs>
+      )}
     </>
   );
 };
