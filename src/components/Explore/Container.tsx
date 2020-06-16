14ea578e883068585103befc18e4e3955ba83e2d
Manuel Spuhler <manuel.spuhler@gmail.com>
2020-06-16 16:50:43 +0200

    !TOS when forbidden Bug #133

--- src/components/Explore/Container.tsx
+++ src/components/Explore/Container.tsx
@@ -77,6 +77,7 @@ export default ({
     if (
       !apiUser.state.loading &&
       apiUser.state.authenticated &&
+      !apiUser.state.forbidden &&
       !apiUser.state.agreeNDA
     ) {
       history.push('/tos');
@@ -84,6 +85,7 @@ export default ({
   }, [
     apiUser.state.agreeNDA,
     apiUser.state.authenticated,
+    apiUser.state.forbidden,
     apiUser.state.loading,
     history
   ]);
