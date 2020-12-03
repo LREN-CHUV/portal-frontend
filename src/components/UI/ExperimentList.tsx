a178d726fbaca55b390390d65010d8a5c53d9982
Manuel Spuhler <Manuel.Spuhler@chuv.ch>
2020-12-03 18:11:26 +0100

    Experiment List icons

--- src/components/UI/ExperimentList.tsx
+++ src/components/UI/ExperimentList.tsx
@@ -1,9 +1,22 @@
+import dayjs from 'dayjs';
+import relativeTime from 'dayjs/plugin/relativeTime';
 import * as React from 'react';
-import { Pagination, Table } from 'react-bootstrap';
+import { Container, Pagination, Table } from 'react-bootstrap';
 import styled from 'styled-components';
-import { IExperimentList, IExperiment } from '../API/Experiment';
+import { IExperiment, IExperimentList } from '../API/Experiment';
+import {
+  BsFillExclamationCircleFill,
+  BsFillEyeFill,
+  BsFillEyeSlashFill,
+  BsCloudDownload,
+  BsFillTrashFill,
+  BsPencilSquare,
+  BsBoxArrowUp
+} from 'react-icons/bs';
+dayjs.extend(relativeTime);
+dayjs().format();
 
-const Container = styled.div`
+const Wrapper = styled(Container)`
   background-color: #fff;
 `;
 
@@ -11,18 +24,38 @@ interface Props {
   experimentList?: IExperimentList;
 }
 
+const ExperimentIcon = ({ experiment }: { experiment: IExperiment }) => {
+  if (experiment.status === 'error') {
+    return <BsFillExclamationCircleFill />;
+  }
+
+  if (experiment.viewed) {
+    return <BsFillEyeFill />;
+  }
+
+  if (!experiment.viewed && experiment.status === 'succes') {
+    return <BsFillEyeSlashFill />;
+  }
+
+  return <BsCloudDownload />;
+};
+
 const Experiment = ({ experiment }: { experiment: IExperiment }) => (
   <tr>
-    <td>{experiment.viewed}</td>
+    <td>
+      <ExperimentIcon experiment={experiment} />
+    </td>
     <td>{experiment.name}</td>
-    <td>{experiment.created}</td>
+    <td>{dayjs().to(dayjs(experiment.created))}</td>
     <td>{experiment.createdBy}</td>
-    <td>share delete rename</td>
+    <td>
+      <BsBoxArrowUp /> <BsFillTrashFill /> <BsPencilSquare />
+    </td>
   </tr>
 );
 
 export default ({ experimentList }: Props) => (
-  <Container>
+  <Wrapper>
     <input placeholder="Search" />
     <Table striped bordered hover>
       <thead>
@@ -46,5 +79,5 @@ export default ({ experimentList }: Props) => (
       <Pagination.Ellipsis />
       <Pagination.Next />
     </Pagination>
-  </Container>
+  </Wrapper>
 );
