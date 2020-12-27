67a7872a6f20f79146d5cb88014bcb5ed25ae7f0
Manuel Spuhler <manuel.spuhlergmail.com>
2020-12-27 16:42:52 +0100

    Pagination Ellipsis

--- src/components/UI/Pagination.tsx
+++ src/components/UI/Pagination.tsx
@@ -1,6 +1,14 @@
+import { encode } from 'querystring';
 import React from 'react';
 import { Pagination } from 'react-bootstrap';
-import { IExperimentList, HandleQueryParameters } from '../API/Experiment';
+import { menuListCSS } from 'react-select/src/components/Menu';
+
+import { HandleQueryParameters, IExperimentList } from '../API/Experiment';
+
+const range = (start: number, end: number): number[] => {
+  const length = end - start;
+  return Array.from({ length }, (_, i) => start + i);
+};
 
 export default ({
   list,
@@ -9,15 +17,21 @@ export default ({
   list: IExperimentList;
   query: HandleQueryParameters;
 }): JSX.Element => {
-  const isSmallList = list.totalPages < 10;
-  const firstNumbers = [...Array(Math.abs(list.currentPage - 5)).keys()];
-  const lastNumbers = [...Array(Math.abs(list.currentPage + 5)).keys()];
+  const nbOFdisplayedItem = 7;
+  const median = 4;
+
+  const start =
+    list.currentPage + 1 <= median
+      ? 2
+      : list.currentPage + 1 >= list.totalPages - median
+      ? list.totalPages - nbOFdisplayedItem
+      : list.currentPage + 2 - median;
+  const end = start + nbOFdisplayedItem;
 
   return (
     <Pagination className="justify-content-center">
-      {!isSmallList && <Pagination.First />}
       <Pagination.Prev
-        disabled={list.currentPage === 0}
+        disabled={list.currentPage <= 0}
         onClick={(): void =>
           query({
             page: list.currentPage - 1
@@ -25,48 +39,50 @@ export default ({
         }
       />
 
-      {isSmallList ? (
-        [...Array(list.totalPages).keys()].map(n => (
-          <Pagination.Item
-            key={`page-${n}`}
-            onClick={(): void => query({ page: n })}
-            active={list.currentPage === n}
-          >
-            {n}
-          </Pagination.Item>
-        ))
-      ) : (
-        <>
-          {firstNumbers.map(n => (
-            <Pagination.Item
-              key={`page-${n}`}
-              onClick={(): void => query({ page: n })}
-              active={list.currentPage === n}
-            >
-              {n}
-            </Pagination.Item>
-          ))}
-          <Pagination.Ellipsis />
-          {lastNumbers.map(n => (
-            <Pagination.Item
-              key={`page-${n}`}
-              onClick={(): void => query({ page: n })}
-              active={list.currentPage === n}
-            >
-              {n}
-            </Pagination.Item>
-          ))}
-        </>
+      {/* First page: 1 */}
+      <Pagination.Item
+        key={`page-0`}
+        onClick={(): void => query({ page: 0 })}
+        active={list.currentPage === 0}
+      >
+        1
+      </Pagination.Item>
+
+      {start + 1 >= median && <Pagination.Ellipsis disabled />}
+
+      {range(start, end).map(n => (
+        <Pagination.Item
+          key={`page-${n}`}
+          onClick={(): void => query({ page: n - 1 })}
+          active={list.currentPage === n - 1}
+        >
+          {n}
+        </Pagination.Item>
+      ))}
+
+      {start + nbOFdisplayedItem < list.totalPages && (
+        <Pagination.Ellipsis disabled />
       )}
+
+      {/* Last page: list.totalPages */}
+      <Pagination.Item
+        key={`page-${list.totalPages}`}
+        onClick={(): void => query({ page: list.totalPages - 1 })}
+        active={list.totalPages === list.currentPage + 1}
+        disabled={list.currentPage + 1 >= list.totalPages}
+      >
+        {list.totalPages}
+      </Pagination.Item>
+
       <Pagination.Next
-        disabled={list.totalPages === list.currentPage + 1}
         onClick={(): void =>
           query({
             page: list.currentPage + 1
           })
         }
+        active={list.totalPages === list.currentPage + 1}
+        disabled={list.currentPage + 1 >= list.totalPages}
       />
-      {!isSmallList && <Pagination.Last />}
     </Pagination>
   );
 };
