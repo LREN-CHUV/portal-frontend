e9109ea0227335bda65440beeb7b31c1fbcd2209
Manuel Spuhler <manuel.spuhlergmail.com>
2020-12-27 20:20:19 +0100

    Pagination, again

--- src/components/UI/Pagination.tsx
+++ src/components/UI/Pagination.tsx
@@ -1,13 +1,89 @@
-import { encode } from 'querystring';
 import React from 'react';
 import { Pagination } from 'react-bootstrap';
-import { menuListCSS } from 'react-select/src/components/Menu';
 
 import { HandleQueryParameters, IExperimentList } from '../API/Experiment';
+
+const PAGE_SIZE = 10; //pageSize (optional) - the number of items per page, defaults to 10
+const MAX_PAGES = 7; // maxPages (optional) - the maximum number of page navigation links to display, defaults to 10
 
 const range = (start: number, end: number): number[] => {
   const length = end - start;
   return Array.from({ length }, (_, i) => start + i);
+};
+
+// https://jasonwatmore.com/post/2018/08/07/javascript-pure-pagination-logic-in-vanilla-js-typescript
+
+const paginate = (
+  totalItems: number,
+  currentPage = 1,
+  pageSize = 10,
+  maxPages = 10
+): {
+  totalItems: number;
+  currentPage: number;
+  pageSize: number;
+  totalPages: number;
+  startPage: number;
+  endPage: number;
+  startIndex: number;
+  endIndex: number;
+  pages: number[];
+} => {
+  // calculate total pages
+  const totalPages = Math.ceil(totalItems / pageSize);
+
+  // ensure current page isn't out of range
+  if (currentPage < 1) {
+    currentPage = 1;
+  } else if (currentPage > totalPages) {
+    currentPage = totalPages;
+  }
+
+  let startPage: number, endPage: number;
+  if (totalPages <= maxPages) {
+    // total pages less than max so show all pages
+    startPage = 1;
+    endPage = totalPages;
+  } else {
+    // total pages more than max so calculate start and end pages
+    const maxPagesBeforeCurrentPage = Math.floor(maxPages / 2);
+    const maxPagesAfterCurrentPage = Math.ceil(maxPages / 2) - 1;
+    if (currentPage <= maxPagesBeforeCurrentPage) {
+      // current page near the start
+      startPage = 1;
+      endPage = maxPages;
+    } else if (currentPage + maxPagesAfterCurrentPage >= totalPages) {
+      // current page near the end
+      startPage = totalPages - maxPages + 1;
+      endPage = totalPages;
+    } else {
+      // current page somewhere in the middle
+      startPage = currentPage - maxPagesBeforeCurrentPage;
+      endPage = currentPage + maxPagesAfterCurrentPage;
+    }
+  }
+
+  // calculate start and end item indexes
+  const startIndex = (currentPage - 1) * pageSize;
+  const endIndex = Math.min(startIndex + pageSize - 1, totalItems - 1);
+
+  // create an array of pages to ng-repeat in the pager control
+  const pages = Array.from(Array(endPage + 1 - startPage).keys()).map(
+    i => startPage + i
+  );
+
+  // return object with all pager properties required by the view
+  return {
+    totalItems,
+    currentPage,
+    pageSize,
+    totalPages,
+    startPage,
+    endPage,
+    startIndex,
+    endIndex,
+    pages
+  };
 };
 
 export default ({
@@ -17,71 +93,42 @@ export default ({
   list: IExperimentList;
   query: HandleQueryParameters;
 }): JSX.Element => {
-  const nbOFdisplayedItem = 7;
-  const median = 4;
-
-  const start =
-    list.currentPage + 1 <= median
-      ? 2
-      : list.currentPage + 1 >= list.totalPages - median
-      ? list.totalPages - nbOFdisplayedItem
-      : list.currentPage + 2 - median;
-  const end = start + nbOFdisplayedItem;
+  const pagination = paginate(
+    list.totalExperiments,
+    list.currentPage + 1,
+    PAGE_SIZE,
+    MAX_PAGES
+  );
 
   return (
     <Pagination className="justify-content-center">
       <Pagination.Prev
-        disabled={list.currentPage <= 0}
+        disabled={pagination.currentPage <= 1}
         onClick={(): void =>
           query({
-            page: list.currentPage - 1
+            page: pagination.currentPage - 2
           })
         }
       />
 
-      {/* First page: 1 */}
-      <Pagination.Item
-        key={`page-0`}
-        onClick={(): void => query({ page: 0 })}
-        active={list.currentPage === 0}
-      >
-        1
-      </Pagination.Item>
-
-      {start + 1 >= median && <Pagination.Ellipsis disabled />}
-
-      {range(start, end).map(n => (
+      {pagination.pages.map((n: number) => (
         <Pagination.Item
           key={`page-${n}`}
           onClick={(): void => query({ page: n - 1 })}
-          active={list.currentPage === n - 1}
+          active={pagination.currentPage === n}
         >
           {n}
         </Pagination.Item>
       ))}
 
-      {start + nbOFdisplayedItem < list.totalPages && (
-        <Pagination.Ellipsis disabled />
-      )}
-
-      {/* Last page: list.totalPages */}
-      <Pagination.Item
-        key={`page-${list.totalPages}`}
-        onClick={(): void => query({ page: list.totalPages - 1 })}
-        active={list.totalPages === list.currentPage + 1}
-        disabled={list.currentPage + 1 >= list.totalPages}
-      >
-        {list.totalPages}
-      </Pagination.Item>
-
       <Pagination.Next
         onClick={(): void =>
           query({
-            page: list.currentPage + 1
+            page: pagination.currentPage
           })
         }
-        active={list.totalPages === list.currentPage + 1}
-        disabled={list.currentPage + 1 >= list.totalPages}
+        active={pagination.totalPages === pagination.currentPage}
+        disabled={pagination.currentPage >= pagination.totalPages}
       />
     </Pagination>
   );
