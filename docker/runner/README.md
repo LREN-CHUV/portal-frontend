c29feec84e921e7656b517211fe9067c1e6cb84b
Nathalie Casati <nathalie.casati@chuv.ch>
2021-06-29 12:57:34 +0000

    adding caddy webserver

--- docker/runner/README.md
+++ docker/runner/README.md
@@ -4,15 +4,12 @@
 
 ## Docker image for Portal Frontend
 
-This Docker image provides a Nginx server configured to serve the frontend HTML, images and scripts
+This Docker image provides a Caddy server configured to serve the frontend HTML, images and scripts
 and provide an access point to the public backend services.
 
 Environment variables:
 
-- WORKER_PROCESSES: number of Nginx worker processes. Should equal the number of CPUs made available to the Docker container
 - ERROR_LOG_LEVEL: reporting level for the error log. Defaults to warn.
-- USE_GZIP: true or false
-- PORTAL_VIRTUAL_HOST: virtual host for the portal application, for example PORTAL_VIRTUAL_HOST="mip.humanbrainproject.eu". Defaults to 'frontend'
 - PORTAL_BACKEND_SERVER: address of the portal backend server, for example PORTAL_BACKEND_SERVER="backend:8080"
 - PORTAL_BACKEND_CONTEXT: context path for the portal backend server, defaults to 'services'.
 - VERSION: Version of MIP
--- docker/runner/run.sh
+++ docker/runner/run.sh
@@ -1,25 +1,6 @@
 #!/usr/bin/env bash
 set -e
 
-export USE_GZIP=${USE_GZIP:-false}
-OPTS="-template /portal/conf/nginx.conf.tmpl:/etc/nginx/nginx.conf"
-OPTS="$OPTS -template /portal/conf/config.json.tmpl:/usr/share/nginx/html/static/config.json"
-# OPTS="$OPTS -template /usr/share/nginx/html/index-tmpl.html:/usr/share/nginx/html/index.html"
+OPTS="-template /portal/conf/config.json.tmpl:/usr/share/caddy/html/static/config.json"
 
-if [ ! -z "$PORTAL_BACKEND_SERVER" ]; then
-  OPTS="$OPTS -template /portal/conf/portal-backend-upstream.conf.tmpl:/etc/nginx/conf.d/portal-backend-upstream.conf"
-  IFS=','
-  for backend in ${PORTAL_BACKEND_SERVER} ; do
-    OPTS="$OPTS -wait tcp://$backend -timeout 100s"
-  done
-  OPTS="$OPTS -template /portal/conf/proxy.conf.tmpl:/etc/nginx/conf.d/proxy.conf"
-fi
-if [ "$USE_GZIP" = "true" ]; then
-  OPTS="$OPTS -template /portal/conf/gzip.conf.tmpl:/etc/nginx/conf.d/gzip.conf"
-else
-  rm -f /etc/nginx/conf.d/gzip.conf
-fi
-
-rm -f /etc/nginx/conf.d/default.conf
-
-eval "exec dockerize $OPTS nginx -g 'daemon off;'"
+eval "exec dockerize $OPTS caddy run --config /etc/caddy/Caddyfile --adapter caddyfile"
